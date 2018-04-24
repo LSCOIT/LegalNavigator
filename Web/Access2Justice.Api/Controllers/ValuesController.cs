9bf81c831540ce1be21c3fd3cc7bcbcb71a0888f
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-04-24 15:50:36 -0400

    implemented DI on all services

--- Web/Access2Justice.Api/Controllers/ValuesController.cs
+++ Web/Access2Justice.Api/Controllers/ValuesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Access2Justice.Api.Models;
 using Access2Justice.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
 
@@ -13,10 +14,25 @@ namespace Access2Justice.Api.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly IConfigurationManager _configurationManager;
+        private readonly IDocumentClient _documentClient;
+
+        public ValuesController(IConfigurationManager configurationManager, IDocumentClient documentClient)
+        {
+            _configurationManager = configurationManager;
+            _documentClient = documentClient;
+        }
+
         // GET api/values
         [HttpGet]
         public async Task<IEnumerable<string>> Get()
         {
+
+            var cosmosDb = new CosmosDbService.CosmosDbService<CuratedExperience>(
+                _documentClient, _configurationManager);
+
+            var itesm = await cosmosDb.GetItemsAsync(x => x.id == "46350752-a33d-3454-2e8d-e1045d554d41");
+
             return new string[] { "value1", "value2" };
         }
 
--- Web/Access2Justice.Api/Startup.cs
+++ Web/Access2Justice.Api/Startup.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Access2Justice.CosmosDbService;
+using Access2Justice.Shared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,11 +23,20 @@ namespace Access2Justice.Api
         }
 
         public IConfiguration Configuration { get; }
+        public object CosmosDbConfiguration { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IConfigurationManager, ConfigurationManager>();
+            services.AddSingleton<IConfigurationBuilder, ConfigurationBuilder>();
+
+            // configure CosmosDb service
+            ICosmosDbConfigurations cosmosDbConfigurations = new CosmosDbConfigurations();
+            Configuration.GetSection("cosmosDb").Bind(cosmosDbConfigurations);
+            IDocumentClient documentClient = new DocumentClient(new Uri(cosmosDbConfigurations.Endpoint), cosmosDbConfigurations.AuthKey);
+            services.AddSingleton(documentClient);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
