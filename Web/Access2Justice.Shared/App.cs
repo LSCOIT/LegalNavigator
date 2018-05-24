e6c8bf335b1871289071744cb0502262ee8eb90f
v-radur <v-radur@H1A-1F155-C5HSX>
2018-05-24 12:35:25 +0530

    Removing search related changes

--- Web/Access2Justice.Shared/App.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Access2Justice.Shared
-{
-   public class App
-    {
-        public string Key { get; set; }
-        public string LuisUrl { get; set; }
-        public int TopIntentsCount { get; set; }
-    }
-}
