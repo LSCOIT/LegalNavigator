71147479c9eea359feecb5fc2049115c74418e21
Turpu Rakesh Reddy (Accenture Services Private Ltd) <v-turake@microsoft.com>
2018-05-25 23:38:27 +0530

    commit after fixing unit test cases for db calls

--- Web/Tests/Access2Justice.Api.Tests/BusinessLogic/TopicsResourcesBusinessLogicTests.cs
+++ Web/Tests/Access2Justice.Api.Tests/BusinessLogic/TopicsResourcesBusinessLogicTests.cs
@@ -1,13 +1,11 @@
 ﻿using Xunit;
 using NSubstitute;
-using System.Collections.Generic;
-using System.Net.Http;
-using Access2Justice.Shared;
 using Access2Justice.Shared.Interfaces;
-using Access2Justice.Api;
 using Newtonsoft.Json.Linq;
 using Access2Justice.CosmosDb.Interfaces;
 using Access2Justice.Api.BusinessLogic;
+using Newtonsoft.Json;
+using System;
 
 namespace Access2Justice.Api.Tests.BusinessLogic
 {
@@ -31,18 +29,81 @@ namespace Access2Justice.Api.Tests.BusinessLogic
 
         }
 
-        //[Fact]
-        //public void GetResourcesAsyncMethod()
-        //{
-        //    //arrange
-        //    string topicIds = "7894654656";
-        //    string query = "select * from t";
-        //    JArray topicsData = JArray.Parse(@"[{'id':'addf41e9-1a27-4aeb-bcbb-7959f95094ba','name':'Family','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257},{'id':'3e278591-ec50-479d-8e38-ae9a9d4cabd9','name':'Eviction','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257},{'id':'3aa3a1be-8291-42b1-85c2-252f756febbc','name':'Housing','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257}]");            
-           
-        //    var dbResponse = _backendDatabaseService.QueryItemsAsync(_cosmosDbSettings.ResourceCollectionId, query);
-        //    dbResponse.Returns(topicsData);
+        [Fact]
+        public void GetTopicAsyncWithProperData()
+        {
+            //arrange
+            string keyword = "eviction";
+            string query = "select * from t";
+            JArray topicsData = JArray.Parse(@"[{'id':'addf41e9-1a27-4aeb-bcbb-7959f95094ba','name':'Family','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257},{'id':'3e278591-ec50-479d-8e38-ae9a9d4cabd9','name':'Eviction','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257},{'id':'3aa3a1be-8291-42b1-85c2-252f756febbc','name':'Housing','parentTopicID':'','keywords':'eviction','location':[{'state':'Hawaii','county':'Kalawao County','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','county':'Honolulu County','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'jsonContent':'','icon':'./assets/images/topics/topic14.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSALHtpAEBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSALHtpAE=/docs/mwoSALHtpAEBAAAAAAAAAA==/','_etag':'\'05008e57-0000-0000-0000-5b0797c10000\'','_attachments':'attachments/','_ts':1527224257}]");
 
-        //    var response = topicsResourcesBusinessLogic.GetResourcesAsync(topicIds);
-        //}
+            var dbResponse = _backendDatabaseService.QueryItemsAsync(_cosmosDbSettings.ResourceCollectionId, query);
+            dbResponse.ReturnsForAnyArgs<dynamic>(topicsData);
+            
+            //act
+            var response = topicsResourcesBusinessLogic.GetTopicAsync(keyword);
+            string result = JsonConvert.SerializeObject(response);
+
+            //assert
+            Assert.Contains("addf41e9-1a27-4aeb-bcbb-7959f95094ba", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void GetTopicAsyncWithEmptyData()
+        {
+            //arrange
+            string keyword = "eviction";
+            string query = "select * from t";
+            JArray topicsData = JArray.Parse(@"[{}]");
+
+            var dbResponse = _backendDatabaseService.QueryItemsAsync(_cosmosDbSettings.ResourceCollectionId, query);
+            dbResponse.ReturnsForAnyArgs<dynamic>(topicsData);
+
+            //act
+            var response = topicsResourcesBusinessLogic.GetTopicAsync(keyword);
+            string result = JsonConvert.SerializeObject(response);
+
+            //assert
+            Assert.Contains("[{}]", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void GetResourcesAsyncWithProperData()
+        {
+            //arrange
+            string keyword = "eviction";
+            string query = "select * from t";
+            JArray resourcesData = JArray.Parse(@"[{'id':'77d301e7-6df2-612e-4704-c04edf271806','name':'Tenant Action Plan for Eviction','description':'This action plan is for tenants who are facing eviction and have experienced the following:','resourceType':'Action','externalUrl':'','url':'','topicTags':[{'id':'f102bfae-362d-4659-aaef-956c391f79de'},{'id':'2c0cc7b8-62b1-4efb-8568-b1f767f879bc'},{'id':'3aa3a1be-8291-42b1-85c2-252f756febbc'}],'location':[{'state':'Hawaii','city':'Kalawao','zipCode':'96742'},{'zipCode':'96741'},{'state':'Hawaii','city':'Honolulu'},{'state':'Hawaii','city':'Hawaiian Beaches'},{'state':'Hawaii','city':'Haiku-Pauwela'},{'state':'Alaska'}],'icon':'./assets/images/resources/resource.png','createdBy':'','createdTimeStamp':'','modifiedBy':'','modifiedTimeStamp':'','_rid':'mwoSAJdNlwIBAAAAAAAAAA==','_self':'dbs/mwoSAA==/colls/mwoSAJdNlwI=/docs/mwoSAJdNlwIBAAAAAAAAAA==/','_etag':'\'040007b5-0000-0000-0000-5b0792260000\'','_attachments':'attachments/','_ts':1527222822}]");
+
+            var dbResponse = _backendDatabaseService.QueryItemsAsync(_cosmosDbSettings.ResourceCollectionId, query);
+            dbResponse.ReturnsForAnyArgs<dynamic>(resourcesData);
+
+            //act
+            var response = topicsResourcesBusinessLogic.GetResourcesAsync(keyword);
+            string result = JsonConvert.SerializeObject(response);
+
+            //assert
+            Assert.Contains("77d301e7-6df2-612e-4704-c04edf271806", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void GetResourcesAsyncWithEmptyData()
+        {
+            //arrange
+            string keyword = "eviction";
+            string query = "select * from t";
+            JArray resourceData = JArray.Parse(@"[{}]");
+
+            var dbResponse = _backendDatabaseService.QueryItemsAsync(_cosmosDbSettings.ResourceCollectionId, query);
+            dbResponse.ReturnsForAnyArgs<dynamic>(resourceData);
+
+            //act
+            var response = topicsResourcesBusinessLogic.GetResourcesAsync(keyword);
+            string result = JsonConvert.SerializeObject(response);
+
+            //assert
+            Assert.Contains("[{}]", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
     }
 }
