f11ac683ae0e3b4d06aab2cc3dc34848741fe74b
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-05-25 17:48:13 -0700

    clean up

--- Web/Access2Justice.Api/Controllers/SearchController.cs
+++ Web/Access2Justice.Api/Controllers/SearchController.cs
@@ -1,7 +1,5 @@
-﻿using Access2Justice.Shared;
-using Access2Justice.Shared.Interfaces;
+﻿using Access2Justice.Shared.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Access2Justice.Api.Controllers
@@ -10,23 +8,17 @@ namespace Access2Justice.Api.Controllers
     [Route("api/search")]
     public class SearchController : Controller
     {
-        private readonly ILuisProxy _luisProxy;
         private readonly ILuisBusinessLogic _luisBusinessLogic;
-        private readonly IWebSearchBusinessLogic _webSearchBusinessLogic;
 
-        public SearchController(ILuisProxy luisProxy,ILuisBusinessLogic luisBusinessLogic, IWebSearchBusinessLogic webSearchBusinessLogic)
+        public SearchController(ILuisBusinessLogic luisBusinessLogic)
         {
-            _luisProxy = luisProxy;
             _luisBusinessLogic = luisBusinessLogic;
-            _webSearchBusinessLogic = webSearchBusinessLogic;
         }
 
         [HttpGet("{query}")]
         public async Task<IActionResult> GetAsync(string query)
         {
             var resources = await _luisBusinessLogic.GetResourceBasedOnThresholdAsync(query);
-
-
             return Content(resources);
         }
     }
