a06e29aab5b22bdb8c0f5921cdeb7050d45154f4
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-05-29 18:15:51 +0530

    updated with basic unit test cases

--- Web/Access2Justice.Web/ClientApp/src/app/shared/location/location.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/location/location.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { LocationComponent } from './location.component';;
+import { } from '@types/bingmaps';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { Injectable, TemplateRef, DebugElement } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { By } from '@angular/platform-browser';
+declare var Microsoft: any;
 
-import { LocationComponent } from './location.component';
 
 describe('LocationComponent', () => {
+  @Injectable()
+  class mockModalService {
+    show: (template) => {}
+  }
+
   let component: LocationComponent;
   let fixture: ComponentFixture<LocationComponent>;
+  let modalService: BsModalService;
+  let template: TemplateRef<any>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LocationComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [],
+        declarations: [LocationComponent],
+        providers: [
+          LocationComponent,
+          { provide: BsModalService, useClass: mockModalService },
+        ]
+      });
+      TestBed.compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LocationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    modalService = fixture.debugElement.injector.get(BsModalService);
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should define component", () => {
+    expect(component).toBeDefined();
+  });
+  
 });
+
