f6c660872aa947cbd33169cd671330e776712236
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-05-30 17:03:04 +0530

    commit

--- Web/Access2Justice.Web/ClientApp/src/app/shared/search-curated-experience/curatedexperience.service.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/search-curated-experience/curatedexperience.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
 import { CuratedExperience } from './curatedexperience';
 import { CuratedExp } from './mock-curated-experience';
 import { HttpClient } from '@angular/common/http';
