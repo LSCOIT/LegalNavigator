282296db2ebc6ca77bf6b488aa085f19c1492f00
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-06-06 16:07:46 +0530

    Implemented Unit test cases

--- Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.spec.ts
+++ Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.spec.ts
@@ -1,48 +1,36 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LocationComponent } from './location.component';
 import { LocationService } from './location.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { Injectable, TemplateRef, DebugElement } from '@angular/core';
+import { TemplateRef, DebugElement } from '@angular/core';
+import { ModalModule } from 'ngx-bootstrap';
 declare var Microsoft: any;
 
-const mockLocationService = {
-  updateLocation: () => { }
-};
-
 describe('LocationComponent', () => {
-  @Injectable()
-  class mockModalService {
-    show: (template) => {}
-  }
-
   let component: LocationComponent;
   let fixture: ComponentFixture<LocationComponent>;
   let modalService: BsModalService;
   let template: TemplateRef<any>;
   let locationService: LocationService;
+  let element: DebugElement;
 
   beforeEach(
-    async(() => {
+    () => {
       TestBed.configureTestingModule({
-        imports: [],
+        imports: [ModalModule.forRoot()],
         declarations: [LocationComponent],
         providers: [
-          LocationComponent,
-          { provide: BsModalService, useClass: mockModalService },
-          { provide: LocationService, useValue: mockLocationService }
+          BsModalService,
+          LocationService
         ]
       });
       TestBed.compileComponents();
-    })
-  );
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LocationComponent);
-    component = fixture.componentInstance;
-    modalService = fixture.debugElement.injector.get(BsModalService);
-    locationService = fixture.debugElement.injector.get(LocationService);
-  });
+      fixture = TestBed.createComponent(LocationComponent);
+      component = fixture.componentInstance;
+      locationService = TestBed.get(LocationService);
+      modalService = TestBed.get(BsModalService);
+    });
 
   it("should create", () => {
     expect(component).toBeTruthy();
@@ -51,6 +39,30 @@ describe('LocationComponent', () => {
   it("should define component", () => {
     expect(component).toBeDefined();
   });
-  
+
+  it("should call locationService getMap when openModal is called", () => {
+    spyOn(locationService, 'getMap');
+    component.openModal(template);
+    expect(locationService.getMap).toHaveBeenCalled();
+  });
+
+  it("should call modalService show when openModal is called", () => {
+    spyOn(locationService, 'getMap');
+    spyOn(modalService, 'show');
+    component.openModal(template);
+    expect(modalService.show).toHaveBeenCalled();
+  });
+
+  //it("should call update location service", () => {
+  //  let mockMapLocation = {
+  //    locality: "Sample Locality",
+  //    address: "Sample Address"
+  //  }
+  //  component.mapLocation = mockMapLocation;
+  //  spyOn(locationService, 'updateLocation');
+  //  component.updateLocation();
+  //  expect(locationService.updateLocation).toHaveBeenCalled();
+  //});
+
 });
 
