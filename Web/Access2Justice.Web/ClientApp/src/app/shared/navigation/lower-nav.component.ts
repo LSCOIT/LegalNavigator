f6d1687ed3fe89482f8c563ba5e0fd6c6d7a8774
Winnie <yfz.winnie@gmail.com>
2018-06-11 17:41:11 -0700

    worked on visual design of home page

--- Web/Access2Justice.Web/ClientApp/src/app/shared/navigation/lower-nav.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/navigation/lower-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-lower-nav',
@@ -6,21 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lower-nav.component.css']
 })
 export class LowerNavComponent implements OnInit {
-  public showMenu = false;
-  constructor() { }
+  @ViewChild('sidenav') sidenav:ElementRef;
+  showSearch = false;
+  my_Class = '';
 
+  constructor() { }
+  
+  openNav() {
+    let width = window.innerWidth;
+    this.my_Class="dimmer";
+    if (width >= 768) {
+      this.sidenav.nativeElement.style.width = "33.333%";
+      this.sidenav.nativeElement.style.height = "100%";
+    } else {
+      this.sidenav.nativeElement.style.height = "100%";
+      this.sidenav.nativeElement.style.width = "100%";
+    }
+  }
+  
+  closeNav() {
+    let width = window.innerWidth;
+    this.my_Class="";
+    if (width >= 768) {
+      this.sidenav.nativeElement.style.width = "0";
+    } else {
+      this.sidenav.nativeElement.style.height = "0";
+    }
+  }
+  
+  toggleSearch() {
+    this.showSearch = !this.showSearch;
+  }
   ngOnInit() {
   }
 
-  getMenu() {
-    if (this.showMenu) {
-      return 'block';
-    } else {
-      return '';
-    }
-  }
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
-  }
 }
