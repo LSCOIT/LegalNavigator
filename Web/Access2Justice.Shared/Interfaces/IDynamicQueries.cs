a60a898d8b1d20d8e1f865c4585201f7c9aae257
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-06-13 18:12:56 -0400

    rename methods to give them 'Async' suffix

--- Web/Access2Justice.Shared/Interfaces/IDynamicQueries.cs
+++ Web/Access2Justice.Shared/Interfaces/IDynamicQueries.cs
@@ -5,9 +5,9 @@ namespace Access2Justice.Shared.Interfaces
 {
     public interface IDynamicQueries
     {
-        Task<dynamic> FindItemsWhere(string collectionId, string propertyName, string value);
-        Task<dynamic> FindItemsWhereContains(string collectionId, string propertyName, string value);
-        Task<dynamic> FindItemsWhereArrayContains(string collectionId, string arrayName, string propertyName, string value);
-        Task<dynamic> FindItemsWhereArrayContains(string collectionId, string arrayName, string propertyName, IEnumerable<string> values);
+        Task<dynamic> FindItemsWhereAsync(string collectionId, string propertyName, string value);
+        Task<dynamic> FindItemsWhereContainsAsync(string collectionId, string propertyName, string value);
+        Task<dynamic> FindItemsWhereArrayContainsAsync(string collectionId, string arrayName, string propertyName, string value);
+        Task<dynamic> FindItemsWhereArrayContainsAsync(string collectionId, string arrayName, string propertyName, IEnumerable<string> values);
     }
 }
