6da080fa242a4a391a08485c2ffeb18e5797ac1a
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-06-14 18:04:31 +0530

    Implemented unit test cases

--- Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/map-results.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/map-results.component.spec.ts
@@ -3,12 +3,44 @@ import { MapResultsComponent } from './map-results.component';
 import { MapResultsService } from './map-results.service';
 import { DebugElement } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 describe('MapResultsComponent', () => {
   let component: MapResultsComponent;
   let fixture: ComponentFixture<MapResultsComponent>;
   let service: MapResultsService;
   let element: DebugElement;
+  let onlyOneOrganization = {
+    resources: [{
+      resourceType: 'organizations',
+      address: 'TestAddress1'
+    }]
+  };
+
+  let onlyTwoOrganization = {
+    resources: [{
+      resourceType: 'organizations',
+      address: 'TestAddress1'
+    },
+    {
+      resourceType: 'organizations',
+      address: 'TestAddress2'
+    }]
+  };
+
+  let nonOrganization = {
+    resources: [{
+      resourceType: 'articles',
+      address: 'TestAddress1'
+    }]
+  };
+
+  let onlyOneLocationCoordinates = {
+    resourceSets: [{
+      resources: [{ point: { coordinates: [111] } }]
+    }]
+  };
+
 
   beforeEach(
     () => {
@@ -30,4 +62,72 @@ describe('MapResultsComponent', () => {
   it("should define component", () => {
     expect(component).toBeDefined();
   });
+
+  it("should call getMap of mapResultsService when getAddress is called", () => {
+    spyOn(service, 'getMap');
+    component.searchResource = 'TestSearchResource';
+    component.getAddress();
+    expect(service.getMap).toHaveBeenCalled();
+  });
+
+  it("should push into organizationsList when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    component.searchResource = onlyOneOrganization;
+    component.getAddress();
+    expect(component.organizationsList.length).toEqual(1);
+  });
+
+  it("should push 2 items into organizations into list when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    component.searchResource = onlyTwoOrganization;
+    component.getAddress();
+    expect(component.organizationsList.length).toEqual(2);
+  });
+
+  it("should not push into organizationsList when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    component.searchResource = nonOrganization;
+    component.getAddress();
+    expect(component.organizationsList.length).toEqual(0);
+  });
+
+  it("should call getMapResults of the component when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    spyOn(component, 'getMapResults').and.returnValue(Observable.of());
+
+    component.searchResource = onlyTwoOrganization;
+    component.getAddress();
+    expect(component.getMapResults).toHaveBeenCalled();
+  });
+
+  it("should call getLocationDetails of service when getMapResults is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    spyOn(service, 'getLocationDetails').and.returnValue(Observable.of());
+
+    component.searchResource = onlyTwoOrganization;
+    component.getAddress();
+    component.getMapResults(onlyTwoOrganization);
+    expect(service.getLocationDetails).toHaveBeenCalled();
+  });
+
+  it("should push item into latitudeLongitude when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    spyOn(service, 'getLocationDetails').and.returnValue(Observable.of(onlyOneLocationCoordinates));
+    spyOn(service, 'mapResults');
+
+    component.searchResource = onlyOneOrganization;
+    component.getAddress();
+    expect(component.latitudeLongitude.length).toEqual(1);
+  });
+
+  it("should push 2 item into latitudeLongitude(subscribe should be called twice) when getAddress is called", () => {
+    spyOn(service, 'getMap').and.returnValue(null);
+    spyOn(service, 'getLocationDetails').and.returnValue(Observable.of(onlyOneLocationCoordinates));
+    spyOn(service, 'mapResults');
+
+    component.searchResource = onlyTwoOrganization;
+    component.getAddress();
+    expect(component.latitudeLongitude.length).toEqual(2);
+  });
+
 });
--- Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/map-results.service.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/map-results.service.spec.ts
@@ -1,9 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MapResultsService } from './map-results.service';
-import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Rx';
 
 describe('MapResults Service', () => {
   let service: MapResultsService;
+  let mockMapResult = {
+    "authenticationResultCode": "ValidCredentials",
+    "brandLogoUri": "http:\/\/dev.virtualearth.net\/Branding\/logo_powered_by.png",
+    "copyright": "Copyright © 2018 Microsoft and its suppliers. All rights reserved. This API cannot be accessed and the content and any results may not be used, reproduced or transmitted in any manner without express written permission from Microsoft Corporation.",
+    "resourceSets": [
+      {
+        "estimatedTotal": 1,
+        "resources": [
+          {
+            "__type": "Location:http:\/\/schemas.microsoft.com\/search\/local\/ws\/rest\/v1",
+
+            "bbox": [21.2903099060059, -157.869766235352, 21.3385944366455, -157.810363769531],
+            "name": "96813, HI", "point": { "type": "Point", "coordinates": [21.3048686981201, -157.858184814453] },
+            "address": { "adminDistrict": "HI", "adminDistrict2": "Honolulu County", "countryRegion": "United States", "formattedAddress": "96813, HI", "locality": "Honolulu", "postalCode": "96813" },
+            "confidence": "High", "entityType": "Postcode1",
+            "geocodePoints": [{
+              "type": "Point",
+              "coordinates": [21.3048686981201, -157.858184814453], "calculationMethod": "Rooftop",
+              "usageTypes": ["Display"]
+            }], "matchCodes": ["Ambiguous"]
+          }]
+      }], "statusCode": 200, "statusDescription": "OK", "traceId": "93c6f159e49040469858aea8b1213926|BN20300154|7.7.0.0|Ref A: 71844B79CA2B49C39955089F6B01A541 Ref B: BLUEDGE0521 Ref C: 2018-06-13T08:42:10Z"
+  }
   const httpSpy = jasmine.createSpyObj('http', ['get']);
   
   beforeEach(() => {
@@ -19,20 +42,17 @@ describe('MapResults Service', () => {
     expect(service).toBeDefined();
   });
 
-  //it('should call getLocationDetails', (done) => {
-  //  let address = "Alaska, United States";
-  //  let credentials = environment.bingmap_key;
+  it('should return map results', (done) => {
+    const mockResponse = Observable.of(mockMapResult);
+    let mockAddress = "Test Address";
+    let mockCredentials = "Test Credentials";
+    httpSpy.get.and.returnValue(mockResponse);
 
-  // //let mockRequest = 'https://dev.virtualearth.net/REST/v1/Locations/' + encodeURI(address) + '?output=json&key=' + credentials;
-  //  //const mockResponse = Observable.of(mockTopics);
-
-  //  //httpSpy.get.and.returnValue(mockResponse);
-  //  spyOn(service.getLocationDetails(address, credentials));
-  //  expect(service.getLocationDetails(address, credentials)).toHaveBeenCalled();
-  //  //service.getLocationDetails(address, credentials).subscribe(result => {
-  //  //  expect(result).toEqual(expect(httpSpy.get).toHaveBeenCalledWith(`${mockRequest}`));
-  //  //  done();
-  //  //});
-  //});
+    service.getLocationDetails(mockAddress, mockCredentials).subscribe(mapResults => {
+      expect(httpSpy.get).toHaveBeenCalled();
+      expect(mapResults).toEqual(mockMapResult);
+      done();
+    });
+  });
 
 });
