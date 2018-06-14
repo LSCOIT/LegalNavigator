28f52a00ef00055dda5691c7d6b37564df295125
Winnie <yfz.winnie@gmail.com>
2018-06-13 17:42:51 -0700

    minor bug fixes for navigation

--- Web/Access2Justice.Web/ClientApp/src/app/shared/navigation/upper-nav.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/navigation/upper-nav.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { ModalModule } from 'ngx-bootstrap';
 import { UpperNavComponent } from './upper-nav.component';
@@ -10,7 +11,8 @@ describe('UpperNavComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UpperNavComponent],
-      imports: [ModalModule.forRoot()]
+      imports: [ModalModule.forRoot()],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -25,8 +27,8 @@ describe('UpperNavComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render five menu items', () => {
+  it('should render four menu items', () => {
     const menuItems = fixture.debugElement.queryAll(By.css('a'));
-    expect(menuItems.length).toBe(5);
+    expect(menuItems.length).toBe(4);
   });
 });
