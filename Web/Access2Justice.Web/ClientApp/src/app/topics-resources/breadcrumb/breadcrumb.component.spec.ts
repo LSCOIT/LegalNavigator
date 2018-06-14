dc48e9be1097426cd1f2a6e192a7c43d872defd5
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-06-14 12:49:30 +0530

    commit unit test cases for breadcrumb component and service

--- Web/Access2Justice.Web/ClientApp/src/app/topics-resources/breadcrumb/breadcrumb.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/topics-resources/breadcrumb/breadcrumb.component.spec.ts
@@ -1,25 +1,118 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { BreadCrumbService } from '../shared/breadcrumb.service';
+import { Observable } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router/src/router_state';
+import { Component } from '@angular/compiler/src/core';
 
-describe('BreadcrumbComponent', () => {
-  let component: BreadcrumbComponent;
-  let fixture: ComponentFixture<BreadcrumbComponent>;
+describe('BreadCrumb Component', () => {
+  let service: BreadCrumbService;
+  let breadcrumbComponent: BreadcrumbComponent;
+  const httpSpy = jasmine.createSpyObj('http', ['get']);
+  let id: string = '19a02209-ca38-4b74-bd67-6ea941d41518';
+  let activatedRoute: ActivatedRoute
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BreadcrumbComponent ]
-    })
-    .compileComponents();
-  }));
-
+  //Testing whether breadcrumb component is created
   beforeEach(() => {
-    fixture = TestBed.createComponent(BreadcrumbComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    breadcrumbComponent = new BreadcrumbComponent(httpSpy, activatedRoute);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create Breadcrumb component', () => {
+    expect(breadcrumbComponent).toBeTruthy();
   });
+
+  //Testing whether breadcrumb service is created
+  beforeEach(() => {
+    service = new BreadCrumbService(httpSpy);
+    httpSpy.get.calls.reset();
+  });
+
+  it('should create Breadcrumb Service', () => {
+    expect(service).toBeTruthy();
+
+  });
+
+  //Testing whether breadcrumb items are retriving via breadcrumb service
+  beforeEach(() => {
+    service = new BreadCrumbService(httpSpy);
+    httpSpy.get.calls.reset();
+  });
+
+  it('should retrieve breadcrumbs from the API via Get', (done) => {
+    let mockBreadcrumb = [
+      { "id": "19a02209-ca38-4b74-bd67-6ea941d41518" }
+    ];
+
+    const mockResponse = Observable.of(mockBreadcrumb);
+
+    httpSpy.get.and.returnValue(mockResponse);
+
+    service.getBreadCrumbs(id).subscribe(topicid => {
+      expect(httpSpy.get).toHaveBeenCalledWith(`${service.getBreadCrumbsUrl}/` + id);
+      expect(topicid).toEqual(mockBreadcrumb);
+      done();
+    });
+  });
+
 });
+
+
+//describe('Breadcrumb Component', () => {
+//  let breadCrumbService: BreadCrumbService;
+//  let breadcrumbComponent: BreadcrumbComponent;
+//  const httpSpy = jasmine.createSpyObj('http', ['get', 'post']);
+//  let activatedRoute: ActivatedRoute
+//  let breadcrumbs = [];
+//  let mockResponse = Observable.of(breadcrumbs)
+//  let activeTopic: "testtopicguid";
+//  let mainTopic: any;
+
+
+//  //Verify Breadcrumb service whether created
+//  beforeEach(() => {
+//    breadCrumbService = new BreadCrumbService(httpSpy);
+//    httpSpy.get.calls.reset();
+//  });
+
+//  it('Breadcrumb Service should create', () => {
+//    expect(breadCrumbService).toBeTruthy();
+//  });
+
+//  //Verify Breadcrumb component whether created
+//  beforeEach(() => {
+//    breadcrumbComponent = new BreadcrumbComponent(breadCrumbService, activatedRoute); //new BreadcrumbComponent(httpSpy);
+//  });
+
+//  it('Breadcrumb Component should create', () => {
+//    expect(breadcrumbComponent).toBeTruthy();
+//  });
+
+
+//  //Verify Breadcrumb - topic doesn't have parent topic
+//  beforeEach(() => {
+
+//    //Get the data for topic from breadcrumb service
+//    breadCrumbService.getBreadCrumbs(activeTopic)
+//      .subscribe(
+//      items => {
+//        breadcrumbs = items.response;
+//      });
+//  });
+//  it('Breadcrumb-topic does not contain parent', () => {
+//    expect(breadcrumbs.length == 1).toBe(true);
+//  });
+
+//  //Verify Breadcrumb - topic has parent topic
+//  beforeEach(() => {
+//    //Get the data for topic from breadcrumb service
+//    breadCrumbService.getBreadCrumbs(activeTopic)
+//      .subscribe(
+//      items => {
+//        breadcrumbs = items.response;
+//      });
+//  });
+//  it('Breadcrumb- topic has parent topics', () => {
+//    expect(breadcrumbs.length > 1).toBe(true)
+//  });
+
+//})
--- Web/Access2Justice.Web/ClientApp/src/app/topics-resources/shared/breadcrumb.service.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/topics-resources/shared/breadcrumb.service.spec.ts
@@ -1,15 +1,29 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { BreadCrumbService } from './breadcrumb.service';
+import { Observable } from 'rxjs/Rx';
 
-describe('BreadCrumbService', () => {
+describe('BreadCrumb Service:', () => {
+  let service: BreadCrumbService;
+  const httpSpy = jasmine.createSpyObj('http', ['get']);
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BreadCrumbService]
-    });
+    service = new BreadCrumbService(httpSpy);
+    httpSpy.get.calls.reset();
   });
 
-  it('should be created', inject([BreadCrumbService], (service: BreadCrumbService) => {
+  it('Breadcrumb Service should be created', () => {
     expect(service).toBeTruthy();
-  }));
+
+  });
+
+  it('should have no breadcrumbs to start', () => {
+    service = new BreadCrumbService(httpSpy);
+    expect(service.getBreadCrumbs.length).toBe(1);
+  });
+
+  it('should retrive root parent breadcrumb', () => {
+    service = new BreadCrumbService(httpSpy);
+    service.getBreadCrumbs("addf41e9-1a27-4aeb-bcbb-7959f95094ba")
+    expect(service.getBreadCrumbs.length).toBe(1);
+  });
+
 });
