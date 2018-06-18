81c3a8f29e53469847af8c7fd7bc703dd696ce52
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-06-18 18:30:54 +0530

    changes related to task#120 for storing searched location details

--- Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.ts
+++ Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { LocationService } from './location.service';
-import { MapLocation, DisplayMapLocation } from './location';
+import { MapLocation } from './location';
 
 @Component({
   selector: 'app-location',
@@ -17,14 +17,14 @@ export class LocationComponent implements OnInit {
   showLocation: boolean = true;
   query: any;
   searchLocation: string;
-  displayMapLocation: DisplayMapLocation;
+  mapLocation: MapLocation;
 
   constructor(private modalService: BsModalService, private locationService: LocationService) {
   }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
-    this.locationService.getMap();
+    this.locationService.getMap(this.mapType);
   }
 
   geocode() {
@@ -34,36 +34,28 @@ export class LocationComponent implements OnInit {
   }
 
   updateLocation() {
-    this.displayMapLocation = this.locationService.updateLocation(this.mapType);
-    //if (this.displayMapLocation.locality !== "" && this.displayMapLocation.address !== "") {
-    //  this.address = this.displayMapLocation.address;
-    //  this.locality = this.displayMapLocation.locality;
-    //  this.showLocation = false;
-    //}
-    this.displayLocationDetails(this.displayMapLocation);
+    this.mapLocation = this.locationService.updateLocation(this.mapType);
+    this.displayLocationDetails(this.mapLocation);
     if (this.modalRef) {
       this.modalRef.hide();
     }
   }
 
-  displayLocationDetails(displayMapLocation) {
-    this.displayMapLocation = displayMapLocation;
-    if (this.displayMapLocation) {
-      this.address = this.displayMapLocation.address;
-      this.locality = this.displayMapLocation.locality;
+  displayLocationDetails(mapLocation) {
+    this.mapLocation = mapLocation;
+    if (this.mapLocation) {
+      this.address = this.mapLocation.address;
+      this.locality = this.mapLocation.locality;
       this.showLocation = false;
     }
   }
 
   ngOnInit() {
-    if (sessionStorage.getItem("globalDisplayMapLocation")) {
-      if (this.mapType === "global") {
-        this.displayMapLocation = JSON.parse(sessionStorage.getItem("globalDisplayMapLocation"));
+    if (sessionStorage.getItem("globalMapLocation")) {
+      if (this.mapType) {
+        this.mapLocation = JSON.parse(sessionStorage.getItem("globalMapLocation"));
       }
-      if (this.mapType === "searchResultsMap") {
-        this.displayMapLocation = JSON.parse(sessionStorage.getItem("searchresultsDisplayMapLocation"));
-      }
-      this.displayLocationDetails(this.displayMapLocation);
+      this.displayLocationDetails(this.mapLocation);
     }
   }
 }
