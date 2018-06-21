711b4ece668d4ec77f3c073e631e63d142cdb758
kapija <kapija@microsoft.com>
2018-06-21 13:26:29 -0700

    Added App Insights logger

---
+++ Web/Access2Justice.Logging/Logging/SAAppSettingsProvider.cs
@@ -0,0 +1,19 @@
+﻿using Access2Justice.Logger.LoggingInterface;
+using MS.IT.FAME.AppLogging.Constants;
+using System.Configuration;
+
+namespace Access2Justice.Logger.Logging
+{
+	class SAAppSettingsProvider : IAppSettingsProvider
+	{
+		
+		public string InstrumentationKey
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings[AppConstants.InstrumentationKey].ToString();
+
+			}
+		}
+	}
+}
---
+++ Web/Access2Justice.Logging/LoggingInterface/IAppFameLogger.cs
@@ -0,0 +1,30 @@
+﻿using Access2Justice.Logger.LoggingInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Access2Justice.Logger
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public interface IAppFameLogger
+	{
+
+		/// <summary>
+		/// This method is used to get logger which help user to Event, Error, Trace, Request.
+		/// </summary>
+		/// <returns></returns>
+		ILogger GetFameLogger();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="inputMessage"></param>
+		/// <param name="correlationId"></param>
+		/// <returns></returns>
+		IDictionary<string, string> SetupBaseLoggingInfo(string inputMessage, string correlationId);
+	}
+}
