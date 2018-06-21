681dec7c83cc2f5d4b835b32a701c34f9abe3657
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-06-21 17:54:11 +0530

    Implemented unit test case for location component

--- Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.spec.ts
+++ Web/Access2Justice.web/ClientApp/src/app/shared/location/location.component.spec.ts
@@ -4,6 +4,7 @@ import { LocationService } from './location.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { TemplateRef } from '@angular/core';
 import { ModalModule } from 'ngx-bootstrap';
+import { MapLocation } from './location';
 
 class MockBsModalRef {
   public isHideCalled = false;
@@ -19,6 +20,14 @@ describe('LocationComponent', () => {
   let modalService: BsModalService;
   let template: TemplateRef<any>;
   let locationService: LocationService;
+  let mockMapLocation: MapLocation = {
+    state: 'Sample State',
+    city : 'Sample City',
+    county : 'Sample County',
+    zipCode: '1009203',
+    locality : 'Sample Location',
+    address : 'Sample Address'
+  };
 
   beforeEach(
     () => {
@@ -45,10 +54,16 @@ describe('LocationComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it("should call locationService getMap when openModal is called", () => {
-    spyOn(locationService, 'getMap');
-    component.openModal(template);
-    expect(locationService.getMap).toHaveBeenCalled();
+  it("should assign session storage details to map location on ngInit", () => {
+    sessionStorage.setItem("mockGlobalMapLocation", JSON.stringify(mockMapLocation));
+    component.mapLocation = JSON.parse(sessionStorage.getItem("mockGlobalMapLocation"));
+  });
+
+  it("should call displayLocationDetails on ngInit", () => {
+    spyOn(component, 'displayLocationDetails');
+    component.ngOnInit();
+    component.displayLocationDetails(mockMapLocation);
+    expect(component.displayLocationDetails).toHaveBeenCalled();
   });
 
   it("should call modalService show when openModal is called", () => {
@@ -58,15 +73,21 @@ describe('LocationComponent', () => {
     expect(modalService.show).toHaveBeenCalled();
   });
 
+  it("should call locationService getMap when openModal is called", () => {
+    spyOn(locationService, 'getMap');
+    component.openModal(template);
+    expect(locationService.getMap).toHaveBeenCalled();
+  });
+  
   it("should call update location of location service when update location of component is called", () => {
-    spyOn(locationService, 'updateLocation').and.returnValue({ locality: '', address: '' });
+    spyOn(locationService, 'updateLocation').and.returnValue(mockMapLocation);
     spyOn(modalService, 'hide');
     component.updateLocation();
     expect(locationService.updateLocation).toHaveBeenCalled();
   });
 
   it("should call hide of modal ref when update location of component is called", () => {
-    spyOn(locationService, 'updateLocation').and.returnValue({ locality: '', address: '' });
+    spyOn(locationService, 'updateLocation').and.returnValue(mockMapLocation);
     spyOn(modalService, 'hide');
     let modalRefInstance = new MockBsModalRef();
     component.modalRef = modalRefInstance;
@@ -74,16 +95,22 @@ describe('LocationComponent', () => {
     expect(modalRefInstance.isHideCalled).toBeTruthy();
   });
 
-  it("should set the address,locality and showLocation variables of component when update location of component is called", () => {
-    let address = 'Sample address';
-    let location = 'Sample location';
-    spyOn(locationService, 'updateLocation').and.returnValue({ locality: location, address: address });
+  it("should call displayLocationDetails when updateLocation is called", () => {
+    spyOn(locationService, 'updateLocation').and.returnValue(mockMapLocation);
+    spyOn(component, 'displayLocationDetails');
+    spyOn(modalService, 'hide');
+    component.updateLocation();
+    expect(component.displayLocationDetails).toHaveBeenCalled();
+  });
+
+  it("should set the address,locality and showLocation variables of component when displayLocationDetails is called", () => {
     spyOn(modalService, 'hide');
     component.showLocation = true;
-    component.updateLocation();
-    expect(component.address).toEqual(address);
-    expect(component.locality).toEqual(location);
+    component.displayLocationDetails(mockMapLocation);
+    expect(component.address).toEqual(mockMapLocation.address);
+    expect(component.locality).toEqual(mockMapLocation.locality);
     expect(component.showLocation).toBeFalsy();
   });
+  
 });
 
