c963f2200570b038b88be5ac9579a2bd847beabe
kapija <kapija@microsoft.com>
2018-06-24 14:51:16 -0700

    updated logic

--- Web/Access2Justice.Logger/Program.cs
+++ Web/Access2Justice.Logger/Program.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Access2Justice.Logger.Logging;
 
 namespace Access2Justice.Logger
@@ -11,23 +8,23 @@ namespace Access2Justice.Logger
     {
         static void Main(string[] args)
         {
-            //Sample code for logging....
-            string key = "<App Insights Instrumentation Key will come here....>";
-            var _logger = AppInsightsLogger.GetInstance(key);
+            //Sample code for logging....         
+            var _properties = new Dictionary<string, string> { { "Search Topic", "Kicked out from home" }, { "Intent Result", "Eviction" } };
+            //pass null for key to pick it up from app.config...or use keyvalut (preferred) to get instrumentation key.
 
-            var properties = new Dictionary<string, string> { { "Method Name", "Main" }, { "Search Topic", "User Keyword" } };
-            _logger.TrackEvent("logging custom event with properties", properties);
-            _logger.FlushInsights();
-
+            var _log = LoggerFactory.GetLogger(AppEnum.LoggerType.AppInsight, null);
+            _log.TrackEvent("TopicSearch", _properties);
+            _log.FlushInsights();
             try
             {
-                Console.WriteLine("Checking exception...");
-                System.IO.DirectoryInfo dr = new System.IO.DirectoryInfo("f:\\dfadfas\\a.txt");
-                dr.CreateSubdirectory("f:\\dfadfas\\a");
+                Console.WriteLine("logging exception...");
+                System.IO.DirectoryInfo dr = new System.IO.DirectoryInfo("j:\\test\\a.txt");
+                dr.CreateSubdirectory("f:\\test\\a");
             }
             catch (Exception ex)
             {
-                _logger.TrackException(ex, "1", AppEnum.EventDictionary.MessageFailedValidation);
+                _log.TrackException(ex, "100", AppEnum.EventDictionary.MessageFailedValidation);
+                _log.FlushInsights();
             }
         }
     }
