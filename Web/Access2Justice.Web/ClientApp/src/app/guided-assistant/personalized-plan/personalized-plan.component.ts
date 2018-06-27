2b02054877dba6ad3e8b926ed4bfffadfcdde124
v-sav <v-sav@microsoft.com>
2018-06-27 14:50:00 +0530

    Updated code to display topics dynamically in personalized and profile pages

--- Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/personalized-plan/personalized-plan.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/personalized-plan/personalized-plan.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PersonalizedPlanService } from '../../profile/personalized-plan/personalized-plan.service';
 import { PersonalizedPlanCondition, Resources, PlanSteps } from '../../profile/personalized-plan/personalized-plan';
 import { ActivatedRoute } from '@angular/router';
+import { NavigateDataService } from '../../shared/navigate-data.service';
 
 @Component({
   selector: 'app-personalized-plan',
@@ -10,42 +11,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PersonalizedPlanComponent implements OnInit {
   conditionsList: Array<PersonalizedPlanCondition>[];
-  activeActionPlan = 'bf8d7e7e-2574-7b39-efc7-83cb94adae07';//this.activeRoute.snapshot.params['id'];
+  activeActionPlan = this.activeRoute.snapshot.params['id'];
   resources: Resources;
-  steps: any;
-  planSteps: Array<PlanSteps> = [];// [{ topicId: '', steps: [] }];
-  planStep: PlanSteps = { topicId: '', steps: [] };
+  topics: string;
 
-  constructor(private personalizedPlanService: PersonalizedPlanService, private activeRoute: ActivatedRoute) { }
+  constructor(private personalizedPlanService: PersonalizedPlanService,
+    private activeRoute: ActivatedRoute,
+    private navigateDataService: NavigateDataService) { }
 
-  getConditions(): void {
+  getTopics(): void {
     this.personalizedPlanService.getActionPlanConditions(this.activeActionPlan)
-      .subscribe(steps => {
-        console.log(steps)
-        if (steps) {
-          this.steps = steps.topicTags;
-          this.steps.forEach(item => {
-            this.planStep.topicId = item.id[0].name;
-            this.planStep.steps = item.stepTags;
-            this.planSteps.push(this.planStep);
-            console.log(this.planSteps);
-          });
-
-          //for (let i = 0; i < this.steps.length; i++) {
-          //  this.planSteps[i].topicId = this.steps[i].id[0].name;
-          //  this.planSteps[i].steps = this.steps[i].stepTags;
-          //}
+      .subscribe(items => {
+        if (items) {
+          this.topics = items.topicTags;
+          this.navigateDataService.setData(items);  
         }
-        else {
-          this.steps = null;
-        }
-        console.log(this.steps);
-        console.log(this.planSteps);
       });
   }
 
   ngOnInit() {
-    this.getConditions();
+    this.getTopics();
   }
 
 }
--- Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Resources } from '../profile/personalized-plan/personalized-plan';
+import { PersonalizedPlanService } from '../profile/personalized-plan/personalized-plan.service';
+import { PersonalizedPlanCondition, Resources, PlanSteps } from '../profile/personalized-plan/personalized-plan';
+import { NavigateDataService } from '../shared/navigate-data.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,11 +10,21 @@ import { Resources } from '../profile/personalized-plan/personalized-plan';
 })
 export class ProfileComponent implements OnInit {
   resources: Resources;
+  topics: string;
 
-  constructor() { }
+  constructor(private personalizedPlanService: PersonalizedPlanService,
+    private navigateDataService: NavigateDataService) { }
 
   getSavedResources(): void {
     this.resources = JSON.parse(sessionStorage.getItem("bookmarkedResource"));
+
+    this.personalizedPlanService.getActionPlanConditions("bf8d7e7e-2574-7b39-efc7-83cb94adae07")//Read data from session
+      .subscribe(items => {
+        if (items) {
+          this.topics = items.topicTags;
+          this.navigateDataService.setData(items);
+        }
+      });
   }
 
   ngOnInit() {
