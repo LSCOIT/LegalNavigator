685a439761091dbd9a9bf0605f0ff7d6d874a8ea
v-chachi <v-chachi@H1A-1F140-C7YSX>
2018-07-11 16:10:36 +0530

    Implemented Added to profile modal popup

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/save-button.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/save-button.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Resources, CreatePlan, StepTag, PlanTag, Steps, ProfileResources } from '../../../guided-assistant/personalized-plan/personalized-plan';
 import { PersonalizedPlanService } from '../../../guided-assistant/personalized-plan/personalized-plan.service';
 import { environment } from '../../../../environments/environment';
 import { UpperNavService } from '../../navigation/upper-nav.service';
+import { Subject } from 'rxjs';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { api } from '../../../../api/api';
 
 @Component({
   selector: 'app-save-button',
   template: `
-<img src="./assets/images/small-icons/save.svg" class="nav-icon" aria-hidden="true" (click) ="savePlanResources()" />
+<img src="./assets/images/small-icons/save.svg" class="nav-icon" aria-hidden="true" (click) ="savePlanResources(template)" />
 <span>Save to profile</span>
- `
+<ng-template #template>
+<div *ngIf="isSavedPlan">
+    <button type="button" class="close" aria-label="Close" (click)="Close()">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    <div class="row text-center">
+        <h3>Added to Profile</h3>
+    </div>
+    <div class="row text-center">
+        <button type="button" (click)="Close()" class="btn btn-primary">Close</button>
+    </div>
+</div>
+</ng-template>
+`
 })
 export class SaveButtonComponent implements OnInit {
   resources: Resources;
   profileResources: ProfileResources = { oId: '', resourceTags: [], type: '' };
-  @Input() savedFrom: string;
   userId: string;
   planId: string;
   createPlan: CreatePlan = { planId: '', oId: '', type: '', planTags: [] };
@@ -25,10 +40,14 @@ export class SaveButtonComponent implements OnInit {
   updatedStep: StepTag = { id: '', order: '', markCompleted: false };
   steps: Array<Steps>;
   planTag: PlanTag = { topicId: '', stepTags: this.steps };
+  isSavedPlan: boolean = false;
+  notifySavePlan: Subject<boolean> = new Subject<boolean>();
+  modalRef: BsModalRef;
 
   constructor(private router: Router,
     private activeRoute: ActivatedRoute,
     private personalizedPlanService: PersonalizedPlanService,
+    private modalService: BsModalService,
     private upperNavService: UpperNavService) {
     let profileData = localStorage.getItem("profileData");
     if (profileData != undefined) {
@@ -37,7 +56,15 @@ export class SaveButtonComponent implements OnInit {
     }
   }
 
-  savePlanResources(): void {
+  externalLogin() {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', api.loginUrl);
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+  savePlanResources(template: TemplateRef<any>): void {
     this.resources = { url: '', itemId: '' };
     this.resources.url = this.router.url;
     if (this.activeRoute.snapshot.params['topic']) {
@@ -47,13 +74,14 @@ export class SaveButtonComponent implements OnInit {
     }
     if (!this.userId) {
       this.personalizedPlanService.saveResourcesToSession(this.resources);
+      this.externalLogin();
     }
     else {
       if (this.resources.url.startsWith("/plan")) {
         this.planId = this.resources.itemId;
-        this.savePlanToProfile();
+        this.savePlanToProfile(template);
       }
-      if (this.resources.url.startsWith("/subtopics")) {
+      if (this.resources.url.startsWith("/subtopics") || this.resources.url.startsWith("/search")) {
         this.personalizedPlanService.getUserPlanId(this.userId)
           .subscribe(response => {
             if (response != undefined) {
@@ -66,13 +94,13 @@ export class SaveButtonComponent implements OnInit {
               });
             }
             this.profileResources.resourceTags.push(this.resources.itemId);
-            this.saveResourceToProfile(this.profileResources.resourceTags);
+            this.saveResourceToProfile(this.profileResources.resourceTags, template);
           });
       }
     }
   }
 
-  savePlanToProfile() {
+  savePlanToProfile(template) {
     this.planTags = [];
     this.personalizedPlanService.getActionPlanConditions(this.planId)
       .subscribe(planDetails => {
@@ -87,14 +115,26 @@ export class SaveButtonComponent implements OnInit {
         });
         this.createPlan = { planId: planDetails.id, oId: this.userId, type: planDetails.type, planTags: this.planTags }
         this.personalizedPlanService.userPlan(this.createPlan)
-          .subscribe();
+          .subscribe(() => {
+            console.log("Saved Successfully");
+            this.isSavedPlan = true;
+            this.modalRef = this.modalService.show(template);
+            });
       });
   }
 
-  saveResourceToProfile(resourceTags) {
+  Close() {
+    this.modalRef.hide();
+    this.router.navigate(['profile']);
+  }
+
+  saveResourceToProfile(resourceTags, template) {
     this.profileResources = { oId: this.userId, resourceTags: resourceTags, type: 'resources' };
     this.personalizedPlanService.userPlan(this.profileResources)
-      .subscribe();
+      .subscribe(() => {
+        this.isSavedPlan = true;
+        this.modalRef = this.modalService.show(template);
+      });
   }
 
   ngOnInit() {
