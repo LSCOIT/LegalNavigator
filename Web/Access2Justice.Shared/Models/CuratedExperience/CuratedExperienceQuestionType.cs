d0d7e63caa88747183631682f68df9b77d02ebef
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-07-12 12:35:26 -0700

    #24 added the question types that A2J Author has

--- Web/Access2Justice.Shared/Models/CuratedExperience/CuratedExperienceQuestionType.cs
+++ Web/Access2Justice.Shared/Models/CuratedExperience/CuratedExperienceQuestionType.cs
@@ -4,11 +4,31 @@ namespace Access2Justice.Shared.Models.CuratedExperience
 {
     public enum CuratedExperienceQuestionType
     {
-        [JsonProperty(PropertyName = "bool")]
-        Bool,
+        [JsonProperty(PropertyName = "text")]
+        Text,
         [JsonProperty(PropertyName = "list")]
         List,
-        [JsonProperty(PropertyName = "textbox")]
-        Textbox
+        [JsonProperty(PropertyName = "number")]
+        Number,
+        [JsonProperty(PropertyName = "currency")]
+        Currency,
+        [JsonProperty(PropertyName = "ssn")]
+        Ssn,
+        [JsonProperty(PropertyName = "phone")]
+        Phone,
+        [JsonProperty(PropertyName = "zipCode")]
+        ZipCode,
+        [JsonProperty(PropertyName = "date")]
+        Date,
+        [JsonProperty(PropertyName = "radioButton")]
+        RadioButton,
+        [JsonProperty(PropertyName = "checkBox")]
+        CheckBox
+
+        // Todo: these data types are from A2J Authors but we don't have equivalent 
+        // for them of at this point. We should find their use case or remove them if
+        // we could manage without them.
+        // Text (Long)
+        // Check Box (None of the Above)
     }
 }
