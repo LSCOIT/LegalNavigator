465de3aee089b44dac7ac09271a9c60d7f5967a7
Turpu Rakesh Reddy (Accenture Services Private Ltd) <v-turake@microsoft.com>
2018-07-17 13:13:09 +0530

    enabling in prod environment file

--- Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
+++ Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4200/api',
-  bingmap_key: 'AqpcQxjuTmheUzCm8b5kUhV9UhjfsK66CtvV2uBvRmBJXZS9FoBvIrnifnZctuaB',
+  bingmap_key: '',
   map_type: false,
   internalResourcePagesToShow: 2,
   internalResourceRecordsToDisplay: 5,
