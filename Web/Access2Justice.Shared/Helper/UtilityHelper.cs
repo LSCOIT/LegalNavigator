418ac4bab2e6b3c0a5d3ead8d5aa74de1d55bd5c
Turpu Rakesh Reddy (Accenture Services Private Ltd) <v-turake@microsoft.com>
2018-07-18 14:32:20 +0530

    commit after implementing encryption for user profile id

--- Web/Access2Justice.Shared/Helper/UtilityHelper.cs
+++ Web/Access2Justice.Shared/Helper/UtilityHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Newtonsoft.Json;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Access2Justice.Shared.Helper
 {
@@ -12,6 +15,24 @@ namespace Access2Justice.Shared.Helper
                 NullValueHandling = NullValueHandling.Ignore,
                 DefaultValueHandling = DefaultValueHandling.Ignore
             };
+        }
+
+        public static string GenerateSHA512String(string inputString)
+        {
+            SHA512 sha512 = SHA512Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha512.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in hash)
+            {
+                result.Append(item.ToString("X2", CultureInfo.InvariantCulture));
+            }
+            return result.ToString();
         }
     }
 }
