b36a445d3ef3b68521f17084ba7cb63fffa124c7
Winnie <yfz.winnie@gmail.com>
2018-07-24 16:07:28 -0700

    added more unit testing

--- Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/curated-experience/curated-experience.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/curated-experience/curated-experience.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CuratedExperienceComponent } from './curated-experience.component';
 
 describe('CuratedExperienceComponent', () => {
@@ -8,7 +8,8 @@ describe('CuratedExperienceComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CuratedExperienceComponent ]
+      declarations: [CuratedExperienceComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -19,7 +20,22 @@ describe('CuratedExperienceComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should receive questions remaining', () => {
+    spyOn(component, 'calculateProgress');
+    component.receiveQuestionsRemaining(9);
+    expect(component.questionsRemaining).toBe(9);
+    expect(component.calculateProgress).toHaveBeenCalled();
+  });
+
+  it('should receive total questions', () => {
+    component.receiveTotalQuestions(9);
+    expect(component.maxProgress).toBe(12);
+  });
+
+  it('should calculate progress', () => {
+    component.questionsRemaining = 5;
+    component.totalQuestions = 12;
+    component.calculateProgress();
+    expect(component.questionProgress).toBe(7);
   });
 });
