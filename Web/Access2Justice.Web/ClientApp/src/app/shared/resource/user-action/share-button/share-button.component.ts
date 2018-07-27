ef68d1115d8716d3ac5f41bed28cb9e360115958
Winnie <yfz.winnie@gmail.com>
2018-07-26 18:39:25 -0700

    created UI for share button modal

---
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/share-button/share-button.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, TemplateRef, Input } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+@Component({
+  selector: 'app-share-button',
+  templateUrl: './share-button.component.html',
+  styleUrls: ['./share-button.component.css']
+})
+export class ShareButtonComponent implements OnInit {
+  @Input() showIcon = true;
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  ngOnInit() {
+  }
+
+}
