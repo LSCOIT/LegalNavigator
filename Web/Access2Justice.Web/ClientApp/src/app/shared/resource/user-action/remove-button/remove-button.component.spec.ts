47e007466dfa60c6dd00a255bee15379fb949e64
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-08-01 18:25:30 +0530

    Issues fixed in profile, upper-nav, remove-button,share-button, topic-resources components

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/remove-button/remove-button.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/remove-button/remove-button.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RemoveButtonComponent } from './remove-button.component';
+import { PersonalizedPlanService } from '../../../../guided-assistant/personalized-plan/personalized-plan.service';
+import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
+import { ArrayUtilityService } from '../../../array-utility.service';
+import { ProfileComponent } from '../../../../profile/profile.component';
+import { EventUtilityService } from '../../../event-utility.service';
+import { PersonalizedPlanComponent } from '../../../../guided-assistant/personalized-plan/personalized-plan.component';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalModule } from 'ngx-bootstrap';
 
 describe('RemoveButtonComponent', () => {
   let component: RemoveButtonComponent;
   let fixture: ComponentFixture<RemoveButtonComponent>;
 
+  //const fakeActivatedRoute = {
+  //  snapshot: { data: { ... } }
+  //} as ActivatedRoute;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RemoveButtonComponent ]
+      imports: [HttpClientModule, RouterTestingModule, ModalModule.forRoot()],
+      declarations: [RemoveButtonComponent],
+      providers: [PersonalizedPlanService, HttpClientModule, HttpClient, HttpHandler, ArrayUtilityService, ProfileComponent, EventUtilityService, PersonalizedPlanComponent, ActivatedRoute]
+      //, useValue: fakeActivatedRoute 
     })
     .compileComponents();
   }));
@@ -19,7 +34,7 @@ describe('RemoveButtonComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
