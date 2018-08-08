b30f86b873f2a747207aa5f7d514b5c21d23095c
v-radur <v-radur@H1A-1F155-C5HSX>
2018-08-08 20:02:34 +0530

    added test cases

--- Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/service-org.service.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/sidebars/service-org.service.ts
@@ -13,20 +13,23 @@ const httpOptions = {
 @Injectable()
 export class ServiceOrgService {
  
-  constructor(private http: HttpClient,
-    private activeRoute: ActivatedRoute,
+  constructor(
+    private http: HttpClient,
     private navigateDataService: NavigateDataService,
     private router: Router,
-    private paginationService: PaginationService) { }
+    private paginationService: PaginationService
+  ) { }
 
   searchResults: any;
   resourceFilter: IResourceFilter = { ResourceType: '', ContinuationToken: '', TopicIds: [], ResourceIds: [], PageNumber: 0, Location: { "state": "", "county": "", "city": "", "zipCode": "" }, IsResourceCountRequired: true };
   topIntent: string;
   activeResource: any;
+
   getOrganizationDetails(location): Observable<any> {
     var objectToSend = JSON.stringify(location);
     return this.http.post<any>(api.getOrganizationDetailsUrl, objectToSend, httpOptions);
   }
+
   clickSeeMoreOrganizations(resourceType: string, activeId: string) {
 
     this.resourceFilter.ResourceType = resourceType;
