d337fee4587d9c4f1a727aa7250652146acf5d48
Winnie <yfz.winnie@gmail.com>
2018-08-10 10:14:40 -0700

    added styling for topic icons and minor edits

--- Web/Access2Justice.Web/ClientApp/src/app/topics-resources/subtopic/subtopics.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/topics-resources/subtopic/subtopics.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { TopicService } from '../shared/topic.service';
 import { ActivatedRoute,Router } from '@angular/router';
 import { NavigateDataService } from '../../shared/navigate-data.service';
-//import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-subtopics',
@@ -15,7 +14,6 @@ export class SubtopicsComponent implements OnInit {
   activeTopic: any;
   activeTopicId: string;
   subtopicName: string;
-  //blobUrl: any = environment.blobUrl;
   topic: any;
   icon: any;
 
@@ -32,7 +30,6 @@ export class SubtopicsComponent implements OnInit {
       .subscribe(
       topic => {
         this.topic = topic[0];
-        //this.icon = this.blobUrl + topic[0].icon;
         this.icon = topic[0].icon;
         });
     this.topicService.getSubtopics(this.activeTopic)
