5bf02500853d5931d7eeb7a0cb4a81f037dc5af5
v-radur <v-radur@H1A-1F155-C5HSX>
2018-08-10 19:32:52 +0530

    test cases added

--- Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.spec.ts
@@ -9,87 +9,87 @@ import { EventUtilityService } from '../shared/event-utility.service';
 import { Tree } from '@angular/router/src/utils/tree';
 import { IResourceFilter } from '../shared/search/search-results/search-results.model';
 
-fdescribe('ProfileComponent', () => {
+xdescribe('profilecomponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-  let personalizedPlanService: PersonalizedPlanService;
-  let arrayUtilityService: ArrayUtilityService;
-  let eventUtilityService: EventUtilityService;
+  let personalizedplanservice: PersonalizedPlanService;
+  let arrayutilityservice: ArrayUtilityService;
+  let eventutilityservice: EventUtilityService;
   
-  const mockTopicService = {
-    getTopics: () => { }
+  const mocktopicservice = {
+    gettopics: () => { }
   };
 
-  let mockPlanId = "dkk2k333";
-  let mockShowRemove = true;
-  let mockPlanDetailsJson = {
+  let mockplanid = "dkk2k333";
+  let mockshowremove = true;
+  let mockplandetailsjson = {
     "id": "bf8d7e7e-2574-7b39-efc7-83cb94adae07",
-    "oId": "User Id",
+    "oid": "user id",
     "type": "plans",
-    "planTags": [{
-        "topicId": "addf41e9-1a27-4aeb-bcbb-7959f95094ba",
-        "stepTags": [
+    "plantags": [{
+        "topicid": "addf41e9-1a27-4aeb-bcbb-7959f95094ba",
+        "steptags": [
           {
             "id": "6b230be1-302b-7090-6cb3-fc6aa084274c",
             "order": 1,
-            "markCompleted": false
+            "markcompleted": false
           },
           {
             "id": "d46aecee-8c79-df1b-4081-1ea02b5022df",
             "order": 2,
-            "markCompleted": false
+            "markcompleted": false
           }
         ],
         "id": "addf41e9-1a27-4aeb-bcbb-7959f95094ba",
       }
     ]
   };
-  let mockTopicList = {
+  let mocktopiclist = {
     "topic":"[{},{}]",
-    "isSelected": true 
+    "isselected": true 
   };
-  let mockTopic = "test";
-  let mockTopics = {
-    "isSelected": true,
+  let mocktopic = "test";
+  let mocktopics = {
+    "isselected": true,
     "topic": {
-      "topicId": "d1d5f7a0-f1fa-464f-8da6-c2e7ce1501ef",
-      "name": "Divorce",
-      "quickLinks": [
+      "topicid": "d1d5f7a0-f1fa-464f-8da6-c2e7ce1501ef",
+      "name": "divorce",
+      "quicklinks": [
         {
-          "Text": "Filing for Dissolution or Divorce - Ending Your Marriage",
-          "Url": "http://courts.alaska.gov/shc/family/shcstart.htm#issues"
+          "text": "filing for dissolution or divorce - ending your marriage",
+          "url": "http://courts.alaska.gov/shc/family/shcstart.htm#issues"
         },
         {
-          "Text": "Spousal Support (Alimony)",
-          "Url": "https://alaskalawhelp.org/resource/spousal-support?ref=OEGQX"
+          "text": "spousal support (alimony)",
+          "url": "https://alaskalawhelp.org/resource/spousal-support?ref=oegqx"
         }
       ],
       "icon": "https://cs4892808efec24x447cx944.blob.core.windows.net/static-resource/assets/images/topics/housing.svg",
       "steps": [
         {
-          "stepId": "66a1e288-4ab1-480f-9d29-eb16abd3ae69"
+          "stepid": "66a1e288-4ab1-480f-9d29-eb16abd3ae69"
         }
       ]
     }
   }
-  let mockUserid = "38890303";
+  let mockuserid = "38890303";
 
-  let mockTopicIds: string[] = [];
-  let mockResourceType = 'test';
-  let mockContinuationToken = 'test';
-  let mockResourceIds = ['test'];
-  let mockLocation = 'test';
-  let mockActiveTopic = "addf41e9-1a27-4aeb-bcbb-7959f95094ba";
+  let mocktopicids: string[] = [];
+  let mockresourcetype = 'test';
+  let mockcontinuationtoken = 'test';
+  let mockresourceids = ['test'];
+  let mocklocation = 'test';
+  let mockactivetopic = "addf41e9-1a27-4aeb-bcbb-7959f95094ba";
   
 
-  let mockResourceInput: IResourceFilter = {
-    ResourceType: mockResourceType,
-    ContinuationToken: mockContinuationToken,
-    TopicIds: mockTopicIds,
+  let mockresourceinput: IResourceFilter = {
+    ResourceType: mockresourcetype,
+    ContinuationToken: mockcontinuationtoken,
+    TopicIds: mocktopicids,
     PageNumber: 1,
-    Location: mockLocation,
+    Location: mocklocation,
     IsResourceCountRequired: false,
-    ResourceIds: mockResourceIds,
+    ResourceIds: mockresourceids,
   };
 
   beforeEach(async(() => {
@@ -97,14 +97,14 @@ fdescribe('ProfileComponent', () => {
       imports: [HttpClientModule],
       declarations: [ProfileComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [PersonalizedPlanService, ArrayUtilityService, EventUtilityService]
+      providers: [personalizedplanservice, arrayutilityservice, eventutilityservice]
     })
       .compileComponents();
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
-    personalizedPlanService = TestBed.get(PersonalizedPlanService);
-    arrayUtilityService = TestBed.get(ArrayUtilityService);
-    eventUtilityService = TestBed.get(EventUtilityService);
+    personalizedplanservice = TestBed.get(personalizedplanservice);
+    arrayutilityservice = TestBed.get(arrayutilityservice);
+    eventutilityservice = TestBed.get(eventutilityservice);
     fixture.detectChanges();
   }));
 
@@ -115,58 +115,58 @@ fdescribe('ProfileComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it('should call getPersonalizedPlan method when component loaded first time in ngOnit', () => {
+  it('should call getpersonalizedplan method when component loaded first time in ngonit', () => {
     component.ngOnInit();
     spyOn(component, 'getPersonalizedPlan');
     component.getPersonalizedPlan();
     component.showRemove;
     expect(component.getPersonalizedPlan).toHaveBeenCalled();
-    expect(component.showRemove).toBe(mockShowRemove);
+    expect(component.showRemove).toBe(mockshowremove);
   });
 
-  it('should call getActionPlanConditions service method when get topics method called', () => {
-    spyOn(personalizedPlanService, 'getActionPlanConditions').and.returnValue(mockPlanDetailsJson);
-    personalizedPlanService.getPersonalizedPlan;
-    component.planId = mockPlanId;
-    expect(personalizedPlanService.getActionPlanConditions(mockPlanId)).toEqual(mockPlanDetailsJson);
-    expect(personalizedPlanService.getActionPlanConditions).toBeTruthy();
+  it('should call getactionplanconditions service method when get topics method called', () => {
+    spyOn(personalizedplanservice, 'getActionPlanConditions').and.returnValue(mockplandetailsjson);
+    personalizedplanservice.getPersonalizedPlan;
+    component.planId = mockplanid;
+    expect(personalizedplanservice.getActionPlanConditions(mockplanid)).toEqual(mockplandetailsjson);
+    expect(personalizedplanservice.getActionPlanConditions).toBeTruthy();
   });
 
-  it('should call filterPlan service method when  topic value is called', () => {
-    spyOn(personalizedPlanService, 'displayPlanDetails');
-    personalizedPlanService.displayPlanDetails(mockPlanDetailsJson, mockTopics);
+  it('should call filterplan service method when  topic value is called', () => {
+    spyOn(personalizedplanservice, 'displayPlanDetails');
+    personalizedplanservice.displayPlanDetails(mockplandetailsjson, mocktopics);
     spyOn(component, 'filterPlan');
-    component.filterPlan(mockTopic);
+    component.filterPlan(mocktopic);
     expect(component.filterPlan).toHaveBeenCalled();
-    expect(personalizedPlanService.displayPlanDetails).toHaveBeenCalled();
+    expect(personalizedplanservice.displayPlanDetails).toHaveBeenCalled();
 
   });
 
-  it('should call getPersonalizedPlan service method when getPersonalizedPlan is called', () => {
+  it('should call getpersonalizedplan service method when getpersonalizedplan is called', () => {
     spyOn(component, 'getPersonalizedPlan');
     component.getPersonalizedPlan();
-    spyOn(personalizedPlanService, 'getPersonalizedPlan');
-    personalizedPlanService.getActionPlanConditions(mockPlanId);
-    expect(personalizedPlanService.getActionPlanConditions(mockPlanId)).toBeTruthy();
+    spyOn(personalizedplanservice, 'getActionPlanConditions');
+    personalizedplanservice.getActionPlanConditions(mockplanid);
+    expect(personalizedplanservice.getActionPlanConditions(mockplanid)).toBeTruthy();
   });
 
-  it('should call getUserSavedResources service method when getpersonalizedResources is called', () => {
+  it('should call getusersavedresources service method when getpersonalizedresources is called', () => {
     component.getpersonalizedResources();
-    spyOn(personalizedPlanService, 'getUserSavedResources');
-    personalizedPlanService.getUserSavedResources(mockUserid);
-    expect(personalizedPlanService.getUserSavedResources).toHaveBeenCalled();
+    spyOn(personalizedplanservice, 'getUserSavedResources');
+    personalizedplanservice.getUserSavedResources(mockuserid);
+    expect(personalizedplanservice.getUserSavedResources).toHaveBeenCalled();
   
   });
 
-  it('should call getPersonalizedResources service method when getSavedResource is called', () =>  {
-    component.getSavedResource(mockResourceInput);
-    spyOn(personalizedPlanService, 'getPersonalizedResources');
-    personalizedPlanService.getPersonalizedResources(mockResourceInput);
-    expect(personalizedPlanService.getPersonalizedResources).toHaveBeenCalled();
+  it('should call getpersonalizedresources service method when getsavedresource is called', () =>  {
+    component.getSavedResource(mockresourceinput);
+    spyOn(personalizedplanservice, 'getPersonalizedResources');
+    personalizedplanservice.getPersonalizedResources(mockresourceinput);
+    expect(personalizedplanservice.getPersonalizedResources).toHaveBeenCalled();
   });
 
-  it('should call filterTopicsList when topic value is passed', () => {
-    component.filterTopicsList(mockTopics);
-    expect(component.filterTopicsList(mockTopics)).toHaveBeenCalled();
+  it('should call filtertopicslist when topic value is passed', () => {
+    component.filterTopicsList(mocktopics);
+    expect(component.filterTopicsList(mocktopics)).toHaveBeenCalled();
   });
 });
