1ae22107d823853a0e08f6f8878c6f8c516a9e50
v-sav <v-sav@microsoft.com>
2018-08-14 18:42:21 +0530

    Removed bing map key value

--- Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
+++ Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4200/api',
-  bingmap_key: 'AlyLqEyg06sSOQJUwYsqcsm69zWmVKYgmaGvqdBPISKCJ59IQqqfCCaQ42KgqTyG',
+  bingmap_key: '',
   map_type: false,
   internalResourcePagesToShow: 2,
   internalResourceRecordsToDisplay: 10,
