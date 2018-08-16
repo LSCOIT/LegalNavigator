53a7178f7a08a588c299dda3ae2994811d35f965
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-08-16 21:42:19 +0530

    83_PersonalizedPlanPDF feature added - partial

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import * as jsPDF from 'jspdf'
 
 @Component({
   selector: 'app-download-button',
   template: `
-  <span>Download</span>
- `
+ <span id="download" (click)="downloadToPDF()">
+ <img src="./assets/images/small-icons/download.svg" class="nav-icon" aria-hidden="true" />
+  Download
+</span>
+ `,
+  providers: [
+    { provide: 'Window', useValue: window }
+  ]
 })
 export class DownloadButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  @Inject('Window') private window: Window;
+    }
 
+  downloadToPDF() {
+   
+    var doc = new jsPDF();
+    doc.text(20, 20, 'Personalized Plan');
+    doc.text(20, 30, 'This is first page.');
+    doc.addPage();
+    doc.text(20, 20, 'This is second page.');
+
+    doc.save('PersonalizedPlan.pdf');
+  }
+  
   ngOnInit() {
   }
+
 
 }
