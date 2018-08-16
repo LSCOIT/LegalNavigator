b473e5f5ce8633740cc3757e41f677e13f64cca4
Soni Rathore <v-sobhad@microsoft.com>
2018-08-16 12:15:46 +0530

    Updated footer contents.

--- Web/Access2Justice.Web/ClientApp/src/app/shared/footer/footer.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { StaticResourceService } from '../static-resource.service';
+import { Navigation, PrivacyPromise, HelpAndFAQ, Home, GuidedAssistant, TopicAndResources, About} from '../navigation/navigation';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private staticResourceService: StaticResourceService) { }
+  blobUrl: any = environment.blobUrl;
+  navigation: Navigation;
+  name: string = 'Navigation';
+  privacyPromise: PrivacyPromise;
+  helpAndFAQ: HelpAndFAQ;
+  home: Home;
+  about: About;
+  guidedAssistant: GuidedAssistant;
+  topicAndResources: TopicAndResources;
 
-  ngOnInit() {
+  filterNavigationContent(navigation): void {
+    if (navigation) {
+      this.privacyPromise = navigation.privacyPromise;
+      this.helpAndFAQ = navigation.helpAndFAQ;
+      this.home = navigation.home;
+      this.about = navigation.about;
+      this.guidedAssistant = navigation.guidedAssistant;
+      this.topicAndResources = navigation.topicAndResources;
+    }
   }
 
+  getNavigationContent(): void {
+    let pageContentRequest = { name: this.name };
+    if (!this.staticResourceService.navigation) {
+      this.staticResourceService.getStaticContent(pageContentRequest)
+        .subscribe(content => {
+          this.navigation = content[0];
+          this.filterNavigationContent(this.navigation);
+          this.staticResourceService.navigation = this.navigation;
+        });
+    }
+    else {
+      this.navigation = this.staticResourceService.navigation;
+      this.filterNavigationContent(this.staticResourceService.navigation);
+    }
+  }
+
+  ngOnInit() {
+    this.getNavigationContent();
+  }
 }
