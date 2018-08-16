b81f6e67b932af13aff2b7fefda1bf70c44cba85
Winnie <yfz.winnie@gmail.com>
2018-08-16 08:00:51 -0700

    separated relevant intents from top intent UI

--- Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/curated-experience-result/curated-experience-result.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/guided-assistant/curated-experience-result/curated-experience-result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NavigateDataService } from '../../shared/navigate-data.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-curated-experience-result',
@@ -10,11 +11,19 @@ export class CuratedExperienceResultComponent implements OnInit {
   @Input() guidedAssistantResults;
   relevantIntents: Array<string>;
 
-  constructor(private navigateDataService: NavigateDataService) { }
+  constructor(
+    private navigateDataService: NavigateDataService,
+    private toastr: ToastrService
+  ) { }
+
+  saveForLater() {
+    this.toastr.success("Topics added. You can view them once you've completed the guided assistant.");
+  }
 
   filterIntent() {
     this.relevantIntents = this.guidedAssistantResults.relevantIntents
-      .filter((resource) => resource.resourceType !== 'None');
+      .filter(resource => resource !== 'None');
+    console.log(this.relevantIntents);
   }
 
   ngOnInit() {
