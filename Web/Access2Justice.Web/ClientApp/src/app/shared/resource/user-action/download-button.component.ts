c8d3a6f2166b30c3e6662eca98f77ba64aefb2a7
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-08-17 22:36:32 +0530

    Added code for pdf generation

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
@@ -8,39 +8,31 @@ import * as jsPDF from 'jspdf'
  <img src="./assets/images/small-icons/download.svg" class="nav-icon" aria-hidden="true" />
   Download
 </span>
- //`,
- // providers: [
- //   { provide: 'Window', useValue: window }
- // ]
+ `,
+
 })
 export class DownloadButtonComponent implements OnInit {
 
-  constructor() {
-  //@Inject('Window') private window: Window;
-    }
+  constructor() {  
+  }
 
   downloadToPDF() {
-   
+
     var doc = new jsPDF();
     var elementHandler = {
       '#ignorePDF': function (element, renderer) {
         return true;
       }
     };
-    var source = window.document.getElementsByTagName("app-personalized-plan")[0];
-    doc.fromHTML(
-      source,
-      15,
-      15,
-      {
-        'width': 180, 'elementHandlers': elementHandler
-      });
-
-    doc.output("dataurlnewwindow");
-  }
   
+    var pdf = new jsPDF();
+    pdf.addHTML(document.body, function () {
+      pdf.save('PersonalisedPlan.pdf');
+    });
+
+  }
+
   ngOnInit() {
   }
-
 
 }
