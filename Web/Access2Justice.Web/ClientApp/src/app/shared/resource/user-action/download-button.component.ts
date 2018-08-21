8b3c9f38e28cfe7e25cd13f9ebe2998ee3d1efe4
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-08-21 15:35:24 +0530

    Generate pdf file feature implemented for Personalized Plan

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import * as jsPDF from 'jspdf'
+import * as filePDF from 'jspdf'
 
 @Component({
   selector: 'app-download-button',
   template: `
- <span id="download" (click)="downloadToPDF()">
+ <span id="download" (click)="downloadFile_PDF()">
  <img src="./assets/images/small-icons/download.svg" class="nav-icon" aria-hidden="true" />
   Download
 </span>
@@ -13,23 +13,35 @@ import * as jsPDF from 'jspdf'
 })
 export class DownloadButtonComponent implements OnInit {
 
-  constructor() {  
+  constructor() {
   }
 
-  downloadToPDF() {
+  downloadFile_PDF() {
 
-    var doc = new jsPDF();
-    var elementHandler = {
-      '#ignorePDF': function (element, renderer) {
+    var pdf_Object = new filePDF('p', 'pt', 'letter');
+    var specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      },
+      '.controls': function (element, renderer) {
         return true;
       }
     };
-  
-    var pdf = new jsPDF();
-    pdf.addHTML(document.body, function () {
-      pdf.save('PersonalisedPlan.pdf');
-    });
 
+    var options = {
+      background: '#fff',
+      format: 'PNG',      
+      margin: {
+        top: 0,
+        bottom: 0
+      },
+      pagesplit: true,
+      'elementHandlers': specialElementHandlers,      
+    };
+    pdf_Object.internal.scaleFactor = 2.25;
+    pdf_Object.addHTML(document.body, options, function () {
+      pdf_Object.save('PersonalisedPlan-' + new Date() + '.pdf');
+    });
   }
 
   ngOnInit() {
