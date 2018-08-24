e529c6e9bb7b37afd56439c7c7c747f4517eb031
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-08-24 19:56:49 +0530

    Modified the generate pdf functionality

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/download-button.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Inject, ElementRef, ViewChild, AfterContentInit } from '@angular/core';
-import * as jsPDF from 'jspdf'
-import { viewParentEl } from '@angular/core/src/view/util';
+import { Component, OnInit, InjectionToken } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-download-button',
@@ -10,84 +9,87 @@ import { viewParentEl } from '@angular/core/src/view/util';
   Download
 </span>
  `,
-
 })
 export class DownloadButtonComponent implements OnInit {
- 
-    //@ViewChild('pdfContent') content: ElementRef;
-  
- 
+  template: string = '';
+  applicationUrl: any = window.location.origin;
+  title: any = document.title;
+  content: any;
+  mainHTML: any;
+  constructor() { }
 
-  
-  constructor() {
+  downloadFile_PDF(): void {
+    if (location.pathname.indexOf("/plan") >= 0) {
+      this.template = 'app-personalized-plan';
+      this.buildContent(this.template);
+      this.savePDF();
+    }
+
   }
 
-  downloadFile_PDF() {
+  buildContent(template) {
 
-    //let doc = new jsPDF();
-    //let specialElementHandlers = {
-    //  '#editor': function (element, renderer) {
-    //    return true;
-    //  }
-    //};
-    ////let content = this.content.nativeElement;
-    //let content = document.getElementsByClassName("pdfContent")[0].innerHTML;
-    //var test = "<html><body>" + content + "</body></html>";
-    //var test1 = "<html><body>Test</body></html>";
-    ////doc.fromHTML(content, 15, 15, {
-    ////  'width': 198,
-    ////  'elementHandlers': specialElementHandlers
-    ////});
+    this.excludeItems();
 
-    //doc.fromHTML(content, 0, 0, {
-    //  'width': 100, // max width of content on PDF
-    //  'elementHandlers': specialElementHandlers
-    //},function (bla) { doc.save('saveInCallback.pdf'); },10);
+    let printContents = document.getElementsByTagName(template)[0].innerHTML;
 
-    //doc.save("test.pdf");
-    //let content = document.getElementsByClassName("pdfContent")[0].outerHTML;
+    this.mainHTML = `
+        <html>
+          <head>
+            <title> ${this.title + ' - ' + this.applicationUrl}</title>
+            <style>
+                @media print {
+                .no-print, .no-print * {
+                  visibility: hidden;
+                  height: 0;
+                }
+                .print-only * {
+                   display: block;
+                }
+                .collapse {
+                    display: block !important;
+                    height: 0 !important;
+                }
+                .hours li span {
+                    margin-right: 10px;
+                }
+              }
+            </style>
+          </head>
+      <body >${printContents}.</body>
+        </html>`
+
+  };
+
+  savePDF() {
 
     var specialElementHandlers = {
       '#editor': function (element, renderer) {
         return true;
       }
     };
-    
-    
-    let content = document.getElementsByClassName("pdfContent")[0].innerHTML;
-    var doc = new jsPDF();//('p', 'pt', 'a4', true);
-    doc.fromHTML(content, 15, 15, {
+
+    var generatePDFDoc = new jsPDF();
+    var imgRegex = new RegExp('<img[^>]*?>', 'g');
+    var content = this.mainHTML.replace(this.mainHTML.match(imgRegex), "");
+
+    generatePDFDoc.fromHTML(content, 15, 15, {
       'width': 170,
       'elementHandlers': specialElementHandlers
     }, function (dispose) {
-      doc.save('thisMotion.pdf');
+      generatePDFDoc.save('PersonalizedPlan.pdf');
     });
 
-    //var pdf_Object = new filePDF('p', 'pt', 'letter');
-    //var specialElementHandlers = {
-    //  '#editor': function (element, renderer) {
-    //    return true;
-    //  },
-    //  '.controls': function (element, renderer) {
-    //    return true;
-    //  }
-    //};
+  }
 
-    //var options = {
-    //  background: '#fff',
-    //  format: 'PNG',
-    //  margin: {
-    //    top: 0,
-    //    bottom: 0
-    //  },
-    //  pagesplit: true,
-    //  'elementHandlers': specialElementHandlers,
-    //};
-
-    //pdf_Object.internal.scaleFactor = 2.25;
-    //pdf_Object.addHTML(document.body, options, function () {
-    //  pdf_Object.save('PersonalisedPlan-' + new Date() + '.pdf');
-    //});
+  excludeItems() {
+    for (var i = document.images.length; i-- > 0;) {
+      document.images[i].parentNode.removeChild(document.images[i]);
+    }
+    document.getElementsByTagName('app-share-button')[0].innerHTML = "";
+    document.getElementsByTagName('app-user-action-sidebar')[0].innerHTML = "";
+    document.getElementsByTagName('app-print-button')[0].innerHTML = "";
+    document.getElementsByTagName('app-download-button')[0].innerHTML = "";
   }
 
   ngOnInit() {
