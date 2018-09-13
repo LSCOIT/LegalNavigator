bab3615e02ccb8f2f5ff9c50d9d22021e0185ff6
Winnie <yfz.winnie@gmail.com>
2018-09-13 14:46:44 -0700

    added static page illustrations

--- Web/Access2Justice.Web/ClientApp/src/app/privacy-promise/privacy-promise.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/privacy-promise/privacy-promise.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PrivacyContent, Image, Details } from '../privacy-promise/privacy-promise';
 import { StaticResourceService } from '../shared/static-resource.service';
 import { Global } from '../global';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-privacy-promise',
@@ -15,28 +16,29 @@ export class PrivacyPromiseComponent implements OnInit {
   name: string = 'PrivacyPromisePage';
   staticContent: any;
   staticContentSubcription: any;
+  blobUrl: string = environment.blobUrl;
 
   constructor(
     private staticResourceService: StaticResourceService,
     private global: Global
   ) { }
 
-  filterPrivacyContent(privacyContent): void {
-    if (privacyContent) {
-      this.informationData = privacyContent.details;
-      this.imageData = privacyContent.image;
-    }
-  }
+  // filterPrivacyContent(privacyContent): void {
+  //   if (privacyContent) {
+  //     this.informationData = privacyContent.details;
+  //     this.imageData = privacyContent.image;
+  //   }
+  // }
 
   getPrivacyPageContent(): void {
     if ((this.staticResourceService.privacyContent) && (this.staticResourceService.privacyContent.location[0].state == this.staticResourceService.getLocation())) {
       this.privacyContent = this.staticResourceService.privacyContent;
-      this.filterPrivacyContent(this.staticResourceService.privacyContent);
+      // this.filterPrivacyContent(this.staticResourceService.privacyContent);
     } else {
       if (this.global.getData()) {
         this.staticContent = this.global.getData();
         this.privacyContent = this.staticContent.find(x => x.name === this.name);
-        this.filterPrivacyContent(this.privacyContent);
+        // this.filterPrivacyContent(this.privacyContent);
         this.staticResourceService.privacyContent = this.privacyContent;
       }
     }
