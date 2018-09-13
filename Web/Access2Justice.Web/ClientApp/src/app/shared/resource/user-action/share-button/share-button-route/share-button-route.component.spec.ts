ee630e596a8452af07c86f441bf6f2caf0a9c9e3
v-radur <v-radur@H1A-1F155-C5HSX>
2018-09-13 18:56:31 +0530

    implemented share button route test cases

--- Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/share-button/share-button-route/share-button-route.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/shared/resource/user-action/share-button/share-button-route/share-button-route.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, TemplateRef } from '@angular/core';
 import { ShareButtonRouteComponent } from './share-button-route.component';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,20 +12,42 @@ import { ModalModule } from 'ngx-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { ShareView } from '../share.model';
+class MockBsModalRef {
+  public isHideCalled = false;
 
+  hide() {
+    this.isHideCalled = true;
+  }
+}
 describe('ShareButtonRouteComponent', () => {
   let component: ShareButtonRouteComponent;
   let fixture: ComponentFixture<ShareButtonRouteComponent>;
-  let mockRouter;
+  let modalService: BsModalService;
+  let mockRouter=
+  {
+      navigate: jasmine.createSpy('navigate'),
+      navigateByUrl: jasmine.createSpy('navigateByUrl')
+  };
   let mockGlobal;
-  let mockShareService: ShareService;
+  let mockShareService;
+  let template: TemplateRef<any>;
   let mockToastr;
   let mockShareButtonRouteComponent;
+  let mockPermalink = "9C87441";
+  let mockResponse= {
+    "userId": '',
+    "userName": '',
+    "resourceLink": "/topics/9c9a59cc-34ac-4a6f-80c4-90ac041abba7"
+  }
 
+  let mockProfileData: ShareView = { UserId: '', UserName: '', IsShared: true };
   beforeEach(async(() => {
     mockToastr = jasmine.createSpyObj(['success']);
     mockShareService = jasmine.createSpyObj(['getResourceLink']);
-
+    mockShareService.getResourceLink.and.returnValue(of(mockResponse));
+   
     TestBed.configureTestingModule({
       imports: [
         BrowserModule,
@@ -42,8 +64,7 @@ describe('ShareButtonRouteComponent', () => {
         { provide: Global, useValue: { role: '', shareRouteUrl: '' } },
         { provide: ActivatedRoute, useValue: { snapshot: { params: { 'id': '123' } } } },
         { provide: Router, useValue: mockRouter },
-        { provide: ShareService, useValue: mockShareService },
-        { provide: Router, useValue: mockRouter }
+        { provide: ShareService, useValue: mockShareService }
       ],
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
@@ -79,10 +100,31 @@ describe('ShareButtonRouteComponent', () => {
       .and.callFake(mockSessionStorage.clear);
   }));
 
-  it('should create test', () => {
+  fit('should create test', () => {
     expect(component).toBeTruthy();
   });
 
+  fit('should call  getResourceLink method when ngOnit loads first time', () => {
+    spyOn(component, 'getResourceLink');
+    component.getResourceLink();
+    expect(component.getResourceLink).toHaveBeenCalled();
+  });
+
+  fit("should call modalService show when openModal is called", () => {
+    modalService = TestBed.get(BsModalService);
+    spyOn(modalService, 'show');
+    component.openModal(template);
+    expect(modalService.show).toHaveBeenCalled();
+  });
+  fit('should call  getResourceLink method of shareService called', () => {
+    let mocklocationchange = Object({ skipLocationChange: true });
+    component.getResourceLink();
+    component.profileData.IsShared = true;
+    expect(component.profileData.UserId).toBe(mockResponse.userId);
+    expect(component.profileData.UserName).toEqual(mockResponse.userName);
+    expect(component.profileData.IsShared).toBe(mockProfileData.IsShared);
+    expect(mockRouter.navigateByUrl).toHaveBeenCalledWith(mockResponse.resourceLink, mocklocationchange);
+  });
 });
 
 
