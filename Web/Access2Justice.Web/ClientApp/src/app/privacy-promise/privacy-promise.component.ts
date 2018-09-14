92953b7c8bd898235b34b303d7c62bf2f11845ca
Winnie <yfz.winnie@gmail.com>
2018-09-14 15:03:46 -0700

    fixed unit test

--- Web/Access2Justice.Web/ClientApp/src/app/privacy-promise/privacy-promise.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/privacy-promise/privacy-promise.component.ts
@@ -23,22 +23,13 @@ export class PrivacyPromiseComponent implements OnInit {
     private global: Global
   ) { }
 
-  // filterPrivacyContent(privacyContent): void {
-  //   if (privacyContent) {
-  //     this.informationData = privacyContent.details;
-  //     this.imageData = privacyContent.image;
-  //   }
-  // }
-
   getPrivacyPageContent(): void {
     if ((this.staticResourceService.privacyContent) && (this.staticResourceService.privacyContent.location[0].state == this.staticResourceService.getLocation())) {
       this.privacyContent = this.staticResourceService.privacyContent;
-      // this.filterPrivacyContent(this.staticResourceService.privacyContent);
     } else {
       if (this.global.getData()) {
         this.staticContent = this.global.getData();
         this.privacyContent = this.staticContent.find(x => x.name === this.name);
-        // this.filterPrivacyContent(this.privacyContent);
         this.staticResourceService.privacyContent = this.privacyContent;
       }
     }
