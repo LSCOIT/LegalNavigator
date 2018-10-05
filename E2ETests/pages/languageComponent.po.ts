10fea9bfef34b003e0e08002a207d7a085037318
Xiao Lin <xiao.lin@avanade.com>
2018-10-05 13:10:37 -0700

    deleted protractor tests

--- E2ETests/pages/languageComponent.po.ts
+++
@@ -1,8 +0,0 @@
-import { $, $$, by, element, until, browser, protractor } from "protractor";
-const chai = require("chai").use(require("chai-as-promised"));
-const expect = chai.expect;
-const assert = chai.assert;
-
-export class LanguageComponent {
-
-} 
--- E2ETests/typeScript/pages/languageComponent.po.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai = require("chai").use(require("chai-as-promised"));
-const expect = chai.expect;
-const assert = chai.assert;
-class LanguageComponent {
-}
-exports.LanguageComponent = LanguageComponent;
