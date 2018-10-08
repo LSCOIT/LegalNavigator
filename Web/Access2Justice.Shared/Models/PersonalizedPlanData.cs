78e4a3f372435097c57b86e372581440db8eee58
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-10-08 17:01:52 -0400

    #414 added a new dto for the personalized plan - CrudePersonalizedPlan

---
+++ Web/Access2Justice.Shared/Models/PersonalizedPlanData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access2Justice.Shared.Models
+{
+    public class CrudePersonalizedPlan
+    {
+        public Guid Id { get; set; }
+        public List<CrudeTopic> CrudeTopics { get; set; }
+    }
+
+    public class CrudeTopic
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Guid> ResourceIds { get; set; }
+    }
+}
