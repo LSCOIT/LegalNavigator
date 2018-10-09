0642b00b4b496f5f7a312e3bfa7c64e9c3179029
Xiao Lin <xiao.lin@avanade.com>
2018-10-09 10:47:54 -0700

    added about page feature

---
+++ Web/Access2Justice.E2ETests/Steps/AboutSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
+using Access2Justice.E2ETests.PageObjects;
+
+namespace Access2Justice.E2ETests.Steps
+{
+    [Binding]
+    public class AboutSteps
+    {
+        SharedObjects SharedObjects = new SharedObjects();
+        About AboutPage = new About();
+
+        [When(@"I click on the About link on the lower navigation bar")]
+        public void WhenIClickOnTheAboutLinkOnTheLowerNavigationBar()
+        {
+            SharedObjects.ClickButton("about-navigation-link");
+        }
+        
+        [Then(@"I should be directed to the About page")]
+        public void ThenIShouldBeDirectedToTheAboutPage()
+        {
+            AboutPage.ConfirmOnAboutPage();
+        }
+    }
+}
