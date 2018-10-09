800e4392c6148b76d749067f925c92a7ecbb271f
Winnie <yfz.winnie@gmail.com>
2018-10-09 11:01:17 -0700

    added test for profile resolver service

--- Web/Access2Justice.Web/ClientApp/src/app/app-resolver/profile-resolver.service.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/app-resolver/profile-resolver.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpParams, HttpClientModule } from '@angular/common/http';
@@ -16,6 +16,7 @@ import { ToastrService } from 'ngx-toastr';
 import { LoginService } from '../shared/login/login.service';
 import { resolve } from 'q';
 import { ProfileResolver } from './profile-resolver.service';
+import { of } from 'rxjs/observable/of';
 
 describe('ProfileResolverService', () => {
   let profileResolverService: ProfileResolver;
@@ -62,11 +63,33 @@ describe('ProfileResolverService', () => {
     expect(profileResolverService).toBeTruthy();
   }));
 
-  it('should resolve be called', (done) => {
-    spyOn(profileResolverService, 'resolve');    
+  it('should resolve be called', fakeAsync(() => {
+    let mockUserData = {
+      displayableId: "mockUser",
+      idToken: {
+        name: "mockUser",
+        preferred_username: "mockUser@microsoft.com"
+      },
+      identityProvider: "https://login.microsoftonline.com/123test",
+      name: "mockUser",
+      userIdentifier: "1234567890ABC"
+    }
+
+    let mockLoginResponse = {
+      oId: "1234567890ABC",
+      name: "mockUser",
+      eMail: "mockUser@microsoft.com"
+    }
+
+    msalService.getUser.and.returnValue(mockUserData);
+    loginService.upsertUserProfile.and.returnValue(of(mockLoginResponse));
     profileResolverService.resolve(mockRoute, mockState);
-    expect(profileResolverService.resolve).toHaveBeenCalled();
-    done();
-  });
+    tick(250);
+    expect(profileResolverService.userProfile.name).toEqual("mockUser");
+    expect(profileResolverService.userProfile.firstName).toBe("");
+    expect(profileResolverService.userProfile.eMail).toEqual("mockUser@microsoft.com");
+    expect(loginService.upsertUserProfile).toHaveBeenCalledWith(profileResolverService.userProfile);
+
+  }));
  
 });
