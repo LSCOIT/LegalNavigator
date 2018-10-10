2432cb78cd522a222815f457a9e91b96c33ee2b6
Turpu Rakesh Reddy (Accenture Services Private Ltd) <v-turake@microsoft.com>
2018-10-10 11:53:48 +0530

    commit after making username label text big

--- Tools/Access2JusticeCode.DataImportTool/DataImport.Designer.cs
+++ Tools/Access2JusticeCode.DataImportTool/DataImport.Designer.cs
@@ -56,11 +56,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(224, 54);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 21);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
             // 
             // DataImport
             // 
