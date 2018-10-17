7ad66176809fd4f40192490cbe4214890ac6a8e7
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-10-17 12:32:24 -0700

    minor optimization

--- Tools/HTMLContentExtractor/Program.cs
+++ Tools/HTMLContentExtractor/Program.cs
@@ -12,9 +12,15 @@ namespace HTMLContentExtractor
             // Pass Json file path name including filename ex: C:\Users\v-turake\Documents\My Received Files\template3.json
             string path = Console.ReadLine();
             string text = LoadJson(path);
-            var splits = ExtractFromString(text, "<legal-nav-resource-id>", "</legal-nav-resource-id>");
-           
-         }
+            var guids = ExtractFromString(text, "<legal-nav-resource-id>", "</legal-nav-resource-id>");
+
+            foreach (var guid in guids)
+            {
+                Console.WriteLine(guid);
+            }
+
+            Console.ReadKey();
+        }
 
         /// <summary>
         /// Reads json file and returns string.
