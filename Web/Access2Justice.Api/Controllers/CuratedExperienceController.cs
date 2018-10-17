de2a9dc37f941fa56e8bad91bf57b70df7da8299
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-10-17 13:47:41 -0700

    #567 created a new controller for the personalized plan and moved all relavent code to it

--- Web/Access2Justice.Api/Controllers/CuratedExperienceController.cs
+++ Web/Access2Justice.Api/Controllers/CuratedExperienceController.cs
@@ -1,6 +1,5 @@
 ﻿using Access2Justice.Api.Interfaces;
 using Access2Justice.Api.ViewModels;
-using Access2Justice.Shared.A2JAuthor;
 using Access2Justice.Shared.Extensions;
 using Access2Justice.Shared.Interfaces.A2JAuthor;
 using Access2Justice.Shared.Models;
@@ -8,10 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization;
-using static Access2Justice.Api.Authorization.Permissions;
-using Access2Justice.Api.Authorization;
 using System.Threading.Tasks;
 
 namespace Access2Justice.Api.Controllers
@@ -21,89 +16,11 @@ namespace Access2Justice.Api.Controllers
     {
         private readonly ICuratedExperienceConvertor a2jAuthorBuisnessLogic;
         private readonly ICuratedExperienceBusinessLogic curatedExperienceBusinessLogic;
-        private readonly IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic;
 
-        public CuratedExperienceController(ICuratedExperienceConvertor a2jAuthorBuisnessLogic, ICuratedExperienceBusinessLogic curatedExperienceBusinessLogic,
-            IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic)
+        public CuratedExperienceController(ICuratedExperienceConvertor a2jAuthorBuisnessLogic, ICuratedExperienceBusinessLogic curatedExperienceBusinessLogic)
         {
             this.a2jAuthorBuisnessLogic = a2jAuthorBuisnessLogic;
             this.curatedExperienceBusinessLogic = curatedExperienceBusinessLogic;
-            this.personalizedPlanBusinessLogic = personalizedPlanBusinessLogic;
-        }
-
-
-        /// <summary>
-        /// This endpoint is just to demo the Curated Experience Personalized Plan schema. Added to help building the UI.
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("personalized-plan/demo")]
-        public IActionResult GetDemoPersonalizedPlan()
-        {
-            //var locations = new List<PlanLocation>();
-            //locations.Add(new PlanLocation
-            //{
-            //    City = "test city",
-            //    State = "test state",
-            //    ZipCode = "11332"
-            //});
-            
-            var quicklinks = new List<PlanQuickLink>();
-            quicklinks.Add(new PlanQuickLink
-            {
-                Text = "Quick link to topic1",
-                Url = new Uri("http://localhost/topic1")
-            });
-
-            //var resources = new List<PlanResource>();
-            //resources.Add(new PlanResource
-            //{
-            //    Description = "Lorem ipsum dolor sit amet, usu soluta aliquid recusabo in, eloquentiam adversarium in vel.",
-            //    ExternalUrl = new Uri("http://localhost"),
-            //    Icon = "",
-            //    IsRecommended = false,
-            //    Location = locations,
-            //    Name = "Resource Name",
-            //    Overview = "Sale oratio tractatos duo et, pri harum senserit mediocritatem an. No eum aliquip menandri.",
-            //    ResourceId = Guid.NewGuid(),
-            //    Tags = new List<string>() { "Test", "Demo" },
-            //    Type = "Organization",
-            //});
-
-            var steps = new List<PlanStep>();
-            steps.Add(new PlanStep
-            {
-                StepId = Guid.NewGuid(),
-                Title = "Plan title",
-                Description = "test plan for building the UI",
-                Order = 1,
-                IsComplete = false,
-                //Resources = resources,
-                Type = "PersonalizedPlan"
-            });
-
-            var topics = new List<PlanTopic>();
-            topics.Add(new PlanTopic
-            {
-                TopicId = Guid.NewGuid(),
-                QuickLinks = quicklinks,
-                Steps = steps
-            });
-
-            return Ok(new PersonalizedPlanViewModel()
-            {
-                PersonalizedPlanId = Guid.NewGuid(),
-                Topics = topics,
-            });
-        }
-
-        [HttpPost("a2j-personalized-plan/parser-test")]
-        public async Task<IActionResult> TestA2JPersonalizedPlanParser([FromBody] CuratedExperienceAnswers userAnswers)
-        {
-            // Todo:@Alaa remove this endpoint, added it just to test the parser duing development
-            var des = curatedExperienceBusinessLogic.FindDestinationComponentAsync(
-                RetrieveCachedCuratedExperience(Guid.Parse("93bcabe2-8afc-4044-b4da-59f7b94510c4")), Guid.Parse("49989209-f89a-4991-86c4-bba41ac2d2d3"), userAnswers.AnswersDocId);
-
-            return Ok(new A2JAuthorLogicParser(new A2JAuthorLogicInterpreter()).Parse(userAnswers));
         }
 
         [HttpPost("import")]
@@ -151,27 +68,6 @@ namespace Access2Justice.Api.Controllers
             return Ok(await curatedExperienceBusinessLogic.GetNextComponentAsync(curatedExperience, component));
         }
 
-        [HttpGet("personalized-plan")]
-        public async Task<IActionResult> GeneratePersonalizedPlan([FromQuery] Guid curatedExperienceId, [FromQuery] Guid answersDocId)
-        {
-            var personalizedPlan = await personalizedPlanBusinessLogic.GeneratePersonalizedPlanAsync(
-                RetrieveCachedCuratedExperience(curatedExperienceId), answersDocId);
-            if (personalizedPlan == null)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-
-            return Ok(personalizedPlan);
-        }
-
-        [Permission(PermissionName.updateplan)]
-        [HttpPost("update-plan")]
-        public async Task<IActionResult> UpdateUserProfileDocumentAsync([FromBody]UserPersonalizedPlan userPlan)
-        {
-            var personalizedPlan = await personalizedPlanBusinessLogic.UpdatePersonalizedPlan(userPlan);
-            return Ok(personalizedPlan);
-        }
-
         private CuratedExperience RetrieveCachedCuratedExperience(Guid id)
         {
             var cuExSession = HttpContext.Session.GetString(id.ToString());
@@ -182,29 +78,6 @@ namespace Access2Justice.Api.Controllers
             }
 
             return HttpContext.Session.GetObjectAsJson<CuratedExperience>(id.ToString());
-        }
-
-        // Todo:@Alaa remove
-        [HttpGet]
-        [Route("get-plan-details/{id}")]
-        public async Task<IActionResult> GetPlanDetailsAsync([FromQuery] Guid curatedExperienceId, [FromQuery] Guid answersDocId)
-        {
-            var personalizedPlan = await personalizedPlanBusinessLogic.GeneratePersonalizedPlanAsync(
-                RetrieveCachedCuratedExperience(curatedExperienceId), answersDocId);
-            if (personalizedPlan == null)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-
-            return Ok(personalizedPlan);
-        }
-
-        [HttpGet]
-        [Route("get-plan/{id}")]
-        public async Task<IActionResult> GetPlanAsync(string id)
-        {
-            var actionPlans = await personalizedPlanBusinessLogic.GetPersonalizedPlan(id);
-            return Ok(actionPlans);
         }
     }
 }
--- Web/Access2Justice.Api/Controllers/PersonalizedPlanController.cs
+++ Web/Access2Justice.Api/Controllers/PersonalizedPlanController.cs
@@ -1,32 +1,108 @@
-﻿using Access2Justice.Api.Interfaces;
+﻿using Access2Justice.Api.Authorization;
+using Access2Justice.Api.Interfaces;
+using Access2Justice.Shared.A2JAuthor;
 using Access2Justice.Shared.Interfaces;
 using Access2Justice.Shared.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
+using static Access2Justice.Api.Authorization.Permissions;
 
 namespace Access2Justice.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/personalized-plan")]
     public class PersonalizedPlanController : Controller
     {
         private readonly ICosmosDbSettings cosmosDbSettings;
         private readonly IBackendDatabaseService backendDatabaseService;
         private readonly IPersonalizedPlanViewModelMapper personalizedPlanViewModelMapper;
+        private readonly IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic;
 
         public PersonalizedPlanController(ICosmosDbSettings cosmosDbSettings, IBackendDatabaseService backendDatabaseService, 
-            IPersonalizedPlanViewModelMapper personalizedPlanViewModelMapper)
+            IPersonalizedPlanViewModelMapper personalizedPlanViewModelMapper, IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic)
         {
             this.cosmosDbSettings = cosmosDbSettings;
             this.backendDatabaseService = backendDatabaseService;
             this.personalizedPlanViewModelMapper = personalizedPlanViewModelMapper;
+            this.personalizedPlanBusinessLogic = personalizedPlanBusinessLogic;
         }
 
-        [HttpGet("test-personalized-plan-ui")]
+        [HttpGet("demo")]
         public async Task<IActionResult> GetPersonalizedPlan()
         {
             var unprocessedPlan = await backendDatabaseService.GetItemAsync<UnprocessedPersonalizedPlan>("ee9a0e48-1855-e236-9f45-485f388a53ae",
                 cosmosDbSettings.PersonalizedActionPlanCollectionId);
 			return Ok(await personalizedPlanViewModelMapper.MapViewModel(unprocessedPlan));
         }
+
+        // Todo:@Alaa remove this endpoint, added it just to test the parser duing development
+        [HttpPost("parser-test")]
+        public async Task<IActionResult> TestA2JAuthorLogicParser([FromBody] CuratedExperienceAnswers userAnswers)
+        {
+            //var des = curatedExperienceBusinessLogic.FindDestinationComponentAsync(
+            //    RetrieveCachedCuratedExperience(Guid.Parse("93bcabe2-8afc-4044-b4da-59f7b94510c4")), Guid.Parse("49989209-f89a-4991-86c4-bba41ac2d2d3"), userAnswers.AnswersDocId);
+
+            return Ok(new A2JAuthorLogicParser(new A2JAuthorLogicInterpreter()).Parse(userAnswers));
+        }
+
+        // Todo:@Alaa remove
+        [HttpGet]
+        [Route("get-plan-details/{id}")]
+        public async Task<IActionResult> GetPlanDetailsAsync([FromQuery] Guid curatedExperienceId, [FromQuery] Guid answersDocId)
+        {
+            var personalizedPlan = new object();// await personalizedPlanBusinessLogic.GeneratePersonalizedPlanAsync(
+                // RetrieveCachedCuratedExperience(curatedExperienceId), answersDocId);
+            if (personalizedPlan == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(personalizedPlan);
+        }
+
+        [HttpGet]
+        [Route("get-plan/{id}")]
+        public async Task<IActionResult> GetPlanAsync(string id)
+        {
+            var actionPlans = await personalizedPlanBusinessLogic.GetPersonalizedPlan(id);
+            return Ok(actionPlans);
+        }
+
+
+        [HttpGet("personalized-plan")]
+        public async Task<IActionResult> GeneratePersonalizedPlan([FromQuery] Guid curatedExperienceId, [FromQuery] Guid answersDocId)
+        {
+            var personalizedPlan = new object();
+                //await personalizedPlanBusinessLogic.GeneratePersonalizedPlanAsync(
+                // RetrieveCachedCuratedExperience(curatedExperienceId), answersDocId);
+            if (personalizedPlan == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(personalizedPlan);
+        }
+
+        [Permission(PermissionName.updateplan)]
+        [HttpPost("update-plan")]
+        public async Task<IActionResult> UpdateUserProfileDocumentAsync([FromBody]UserPersonalizedPlan userPlan)
+        {
+            var personalizedPlan = await personalizedPlanBusinessLogic.UpdatePersonalizedPlan(userPlan);
+            return Ok(personalizedPlan);
+        }
+
+        // // Todo:@Alaa factor this out, i copied it from the CuratedExperience controller for now to finish an end-to-end personalized plan
+        //private CuratedExperience RetrieveCachedCuratedExperience(Guid id)
+        //{
+        //    var cuExSession = HttpContext.Session.GetString(id.ToString());
+        //    if (string.IsNullOrWhiteSpace(cuExSession))
+        //    {
+        //        var rawCuratedExperience = curatedExperienceBusinessLogic.GetCuratedExperienceAsync(id).Result;
+        //        HttpContext.Session.SetObjectAsJson(id.ToString(), rawCuratedExperience);
+        //    }
+
+        //    return HttpContext.Session.GetObjectAsJson<CuratedExperience>(id.ToString());
+        //}
     }
 }
