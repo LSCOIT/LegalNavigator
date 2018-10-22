51b2e47f636fea42d15ae04015caa5b4abf5013c
Winnie <yfz.winnie@gmail.com>
2018-10-22 15:24:02 -0700

    temporarily changing personalized plan endpoint

--- Web/Access2Justice.Web/ClientApp/src/api/api.ts
+++ Web/Access2Justice.Web/ClientApp/src/api/api.ts
@@ -15,7 +15,7 @@ export const api = {
     getResourceUrl: apiUrl + '/resources',
     searchOffsetUrl: apiUrl + '/web-search',
     breadcrumbsUrl: topic + '/get-breadcrumbs',
-    personalizedPlan: apiUrl + '/personalized-plan/generate',
+    personalizedPlan: 'https://access2justiceapi.azurewebsites.net/api/personalized-plan/generate',
     planUrl: curatedExperience + "/get-plan-details",
     getPersonalizedResourcesUrl: apiUrl + '/personalized-resources',
     updatePlanUrl: userProfile + "/upsert-user-plan",
