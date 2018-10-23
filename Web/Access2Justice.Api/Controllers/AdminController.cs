73a90a8991330a4a356a3094242269a493e84165
v-sav <v-sav@microsoft.com>
2018-10-23 20:23:31 +0530

    Added logic to append children nodes from children template to main template

--- Web/Access2Justice.Api/Controllers/AdminController.cs
+++ Web/Access2Justice.Api/Controllers/AdminController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Access2Justice.Api.ViewModels;
+using Access2Justice.Shared.Extensions;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
 
@@ -47,17 +53,37 @@ namespace Access2Justice.Api.Controllers
                     {
                         file.CopyTo(stream);
                     }
-                    
+
                     if (!newPath.EndsWith(Path.DirectorySeparatorChar))
                         newPath += Path.DirectorySeparatorChar;
+
+                    var templateOrder = new List<JToken>();
+                    List<CuratedExperiencePackageTemplateModel> templateModel = new
+                        List<CuratedExperiencePackageTemplateModel>();
 
                     using (ZipArchive archive = ZipFile.OpenRead(fullPath))
                     {
                         string destinationPath = string.Empty;
                         foreach (ZipArchiveEntry entry in archive.Entries)
                         {
+                            if (entry.FullName.Equals("templates.json"))
+                            {
+                                destinationPath = Path.GetFullPath(Path.Combine(newPath, entry.FullName));
+
+                                if (destinationPath.StartsWith(newPath, StringComparison.Ordinal))
+                                    entry.ExtractToFile(destinationPath);
+
+                                using (StreamReader reader = new StreamReader(destinationPath))
+                                {
+                                    string json = reader.ReadToEnd();
+                                    var parser = JObject.Parse(json);
+                                    templateOrder = parser.Properties().GetArrayValue("templateIds")
+                                           .FirstOrDefault().ToList();
+                                }
+                            }
+
                             Match match = Regex.Match(entry.FullName, @"template([0-9\-]+)\.json$",
-                                            RegexOptions.IgnoreCase);
+                                        RegexOptions.IgnoreCase);
 
                             if (match.Success)
                             {
@@ -65,18 +91,57 @@ namespace Access2Justice.Api.Controllers
 
                                 if (destinationPath.StartsWith(newPath, StringComparison.Ordinal))
                                     entry.ExtractToFile(destinationPath);
-                            }
-                            //ToDo - Extract child node Json content from Template2,3... and add to template1.json
 
+                                using (StreamReader reader = new StreamReader(destinationPath))
+                                {
+                                    string json = reader.ReadToEnd();
+                                    templateModel.Add(new CuratedExperiencePackageTemplateModel
+                                    {
+                                        TemplateName = Path.GetFileNameWithoutExtension(entry.FullName).Replace("template", ""),
+                                        Template = JObject.Parse(json)
+                                    });
+                                }
+                                //Delete uploaded json file
+                                if (System.IO.File.Exists(destinationPath))
+                                {
+                                    System.IO.File.Delete(destinationPath);
+                                }
+                            }
                         }
                     }
-                    //Delete uploaded files
+
+                    //Delete uploaded zip file
                     if (System.IO.File.Exists(fullPath))
                     {
                         System.IO.File.Delete(fullPath);
                     }
+
+                    var parentTemplateId = templateModel.FirstOrDefault().TemplateName;
+                    JObject mainTemplate = null;
+                    JArray mainTemplateChildren = null;
+
+                    foreach (var model in templateModel)
+                    {
+                        if (model.TemplateName == parentTemplateId)
+                        {
+                            mainTemplate = model.Template;
+                            JObject rootNode = (JObject)mainTemplate["rootNode"];
+                            mainTemplateChildren = (JArray)rootNode["children"];
+                        }
+                        else if (model.TemplateName != parentTemplateId)
+                        {
+                            JObject childTemplate = model.Template; 
+                            JObject childRootNode = (JObject)childTemplate["rootNode"];
+                            JArray childrenNode = (JArray)childRootNode["children"];
+                            foreach(var child in childrenNode)
+                            {
+                                mainTemplateChildren.Add(child.ToString());
+                            }
+                        }
+                    }
+                    return Json("Upload Successful.");
                 }
-                return Json("Upload Successful.");
+                return Json("Please provide the file to upload.");
             }
             catch (System.Exception ex)
             {
