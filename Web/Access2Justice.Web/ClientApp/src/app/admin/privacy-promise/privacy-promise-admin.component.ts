f3f5290bec73b6012389cd39ac4593acd6d919fb
Winnie <yfz.winnie@gmail.com>
2018-10-23 18:03:00 -0700

    add method to convert form field to correct json format

--- Web/Access2Justice.Web/ClientApp/src/app/admin/privacy-promise/privacy-promise-admin.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/admin/privacy-promise/privacy-promise-admin.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { PrivacyContent} from "../../privacy-promise/privacy-promise";
+import { PrivacyContent, Details} from "../../privacy-promise/privacy-promise";
 import { Global } from "../../global";
 import { NgForm } from '@angular/forms';
+import { environment } from '../../../environments/environment';
+import { StaticResourceService } from '../../shared/static-resource.service';
 
 @Component({
   selector: 'app-privacy-promise-admin',
@@ -10,19 +12,59 @@ import { NgForm } from '@angular/forms';
 })
 
 export class PrivacyPromiseAdminComponent implements OnInit {
-  privacyContent: PrivacyContent;
+  detailParams: any;
   newPrivacyContent: any;
+  privacyContent: PrivacyContent;
+  informationData: Array<Details> = [];
+  name: string = 'PrivacyPromisePage';
+  staticContent: any;
+  staticContentSubcription: any;
+  blobUrl: string = environment.blobUrl;
 
   constructor(
+    private staticResourceService: StaticResourceService,
     private global: Global
-    ) { }
+  ) { }
+
+  mapSectionDescription(form) {
+    let numOfTitles = Object.keys(form).filter((key) => key.indexOf("title") > -1);
+
+    this.detailParams = numOfTitles.map((key, i) => {
+        return {
+          title: form[`title${i}`],
+          description: form[`description${i}`]
+      }
+    });
+
+  }
 
   onSubmit(privacyForm: NgForm) {
     this.newPrivacyContent = privacyForm.value;
+    this.mapSectionDescription(privacyForm.value);
+    let params = {
+      "description": privacyForm.value.pageDescription,
+      "details": this.detailParams
+    }
+    console.log(params);
+  }
+
+  getPrivacyPageContent(): void {
+    if ((this.staticResourceService.privacyContent) && (this.staticResourceService.privacyContent.location[0].state == this.staticResourceService.getLocation())) {
+      this.privacyContent = this.staticResourceService.privacyContent;
+    } else {
+      if (this.global.getData()) {
+        this.staticContent = this.global.getData();
+        this.privacyContent = this.staticContent.find(x => x.name === this.name);
+        this.staticResourceService.privacyContent = this.privacyContent;
+      }
+    }
   }
 
   ngOnInit() {
-    this.privacyContent = this.global.getData().find(x => x.name === 'PrivacyPromisePage');
+    this.getPrivacyPageContent();
+    this.staticContentSubcription = this.global.notifyStaticData
+      .subscribe((value) => {
+        this.getPrivacyPageContent();
+      });
   }
-
 }
