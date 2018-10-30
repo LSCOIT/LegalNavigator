885b16c9cccdb440f22fe64bc4cbdc6fb4cdbcce
Alaa Tadmori <alaa.tadmori@avanade.com>
2018-10-29 18:28:57 -0700

    refacotred and clean up the session store code into a SessionManger class

---
+++ Web/Access2Justice.Api/Interfaces/ISessionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Access2Justice.Shared.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Access2Justice.Api.Interfaces
+{
+    public interface ISessionManager
+    {
+        CuratedExperience RetrieveCachedCuratedExperience(Guid id, HttpContext context);
+    }
+}
