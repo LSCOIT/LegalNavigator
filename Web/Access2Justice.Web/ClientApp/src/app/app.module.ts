3951bdd7edb79bc989bf9de0cf40fffdd04d87e7
yfzwinnie <yfz.winnie@gmail.com>
2018-11-02 11:32:08 -0700

    fixed merge conflict and remove protected resources according to task 721

--- Web/Access2Justice.Web/ClientApp/src/app/app.module.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/app.module.ts
@@ -48,9 +48,6 @@ import { TokenInterceptor } from './token-interceptor';
 import { environment } from '../environments/environment';
 import { api } from '../api/api';
 
-export const protectedResourceMap: [string, string[]][] = [[api.checkPermaLink, [environment.apiScope]], [api.shareUrl, [environment.apiScope]]
-  , [api.unShareUrl, [environment.apiScope]], [api.userPlanUrl, [environment.apiScope]], [api.updatePrivacyDataUrl, [environment.apiScope]]] 
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -82,7 +79,7 @@ export const protectedResourceMap: [string, string[]][] = [[api.checkPermaLink, 
     BsDropdownModule.forRoot(),
     CarouselModule.forRoot(),
     CollapseModule.forRoot(),
-    ModalModule.forRoot(),
+    ModalModule.forRoot(),task
     ProgressbarModule.forRoot(),
     TabsModule.forRoot(),
     NgxSpinnerModule,
