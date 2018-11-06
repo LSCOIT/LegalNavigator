b30dd4c8f1942611878b8b605ad32ee2528aa386
Sateesh Reddy Venna <v-sav@microsoft.com>
2018-11-06 19:20:23 +0530

    Added form validation at client side and added link in admin dashboard componet so that once the admin clicks on the upload curated experience button will appear. Once the user clicks the button it will redirect to the upload page.

--- Web/Access2Justice.Web/ClientApp/src/app/admin/upload-curated-experience-template/upload-curated-experience-template.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/admin/upload-curated-experience-template/upload-curated-experience-template.component.ts
@@ -20,45 +20,50 @@ export class UploadCuratedExperienceTemplateComponent implements OnInit {
   ngOnInit() { }
 
   onSubmit(uploadForm: NgForm) {
-    this.spinner.show();
-    if (uploadForm.form.value) {
-      this.successMessage = "";
-      this.errorMessage = "";
-      let formValue = uploadForm.form.value;
-      let file = this.file.nativeElement.files[0];
-      let formData = new FormData();
-      if (file != null && file.size > 0) {
-        formData.append("templateFile", file);
-        formData.append('name', formValue.name);
-        formData.append('description', formValue.description);
+    if (uploadForm.valid) {
+      this.spinner.show();
+      if (uploadForm.form.value) {
+        this.successMessage = "";
+        this.errorMessage = "";
+        let formValue = uploadForm.form.value;
+        let file = this.file.nativeElement.files[0];
+        let formData = new FormData();
+        if (file != null && file.size > 0) {
+          formData.append("templateFile", file);
+          formData.append('name', formValue.name);
+          formData.append('description', formValue.description);
+        }
+
+        let params = new HttpParams();
+        params.append('Content-Type', 'multipart/form-data;');
+        const options = {
+          params: params
+        };
+
+        this.http.post(api.uploadCuratedExperienceTemplateUrl, formData, { params, responseType: 'text'})
+          .subscribe(
+            response => {
+              this.spinner.hide();
+              uploadForm.reset();
+              this.successMessage = response;
+            },
+            error => {
+              this.spinner.hide();
+              if (error.error) {
+                this.errorMessage = error.error;
+              }
+              else if (error.statusText) {
+                this.errorMessage = error.statusText;
+              }
+              else {
+                this.errorMessage = error
+              }
+              console.log(error);
+            });
       }
-
-      let params = new HttpParams();
-      params.append('Content-Type', 'multipart/form-data;');
-      const options = {
-        params: params
-      };
-
-      this.http.post(api.uploadCuratedExperienceTemplateUrl, formData, { params, responseType: 'text'})
-        .subscribe(
-        response => {
-          this.spinner.hide();
-            uploadForm.reset();
-            this.successMessage = response;
-          },
-        error => {
-          this.spinner.hide();
-          if (error.error) {
-            this.errorMessage = error.error;
-          }
-          else if (error.statusText) {
-            this.errorMessage = error.statusText;
-          }
-          else {
-            this.errorMessage = error
-          }
-          console.log(error);
-        });
+    }
+    else {
+      this.errorMessage = "Please provide the required fields.";
     }
   }
 }
