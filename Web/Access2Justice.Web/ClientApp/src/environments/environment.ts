ba8fb0e40bb105411734219c53f5c58c78c0301e
Winnie <yfz.winnie@gmail.com>
2018-11-06 08:58:45 -0800

    changed environment api endpoint back to local host

--- Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
+++ Web/Access2Justice.Web/ClientApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  apiUrl: 'https://access2justiceapi.azurewebsites.net/api',
+  apiUrl: 'http://localhost:4200/api',
   bingmap_key: 'AlyLqEyg06sSOQJUwYsqcsm69zWmVKYgmaGvqdBPISKCJ59IQqqfCCaQ42KgqTyG',
   map_type: false,
   internalResourcePagesToShow: 2,
