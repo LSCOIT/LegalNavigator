ed2ccfc3b521a28b203e9cdc434af0dcf28597b2
Sateesh Reddy Venna <v-sav@microsoft.com>
2018-11-09 18:06:44 +0530

    Added unit test for null check validation

--- Web/Access2Justice.Web/ClientApp/src/app/admin/upload-curated-experience-template/upload-curated-experience-template.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/admin/upload-curated-experience-template/upload-curated-experience-template.component.spec.ts
@@ -1,17 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { UploadCuratedExperienceTemplateComponent } from './upload-curated-experience-template.component';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { UploadCuratedExperienceTemplateComponent } from './upload-curated-experience-template.component';
 
 describe('UploadCuratedExperienceTemplateComponent', () => {
   let component: UploadCuratedExperienceTemplateComponent;
   let fixture: ComponentFixture<UploadCuratedExperienceTemplateComponent>;
   let mockNgxSpinnerService;
   let mockRouter;
-
+  let mockErrorMessage;
+  
   beforeEach(async(() => {
+    mockErrorMessage = "Please provide the required fields.";
     TestBed.configureTestingModule({
       declarations: [UploadCuratedExperienceTemplateComponent],
       imports: [FormsModule, HttpClientModule],
@@ -31,5 +33,17 @@ describe('UploadCuratedExperienceTemplateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not submit without required field values', () => {
+    let mockUploadForm = <NgForm>{
+      value: {
+        name: "Divorce",
+        description: "Divorce Demo",
+        file: null
+      }
+    }
+    component.onSubmit(mockUploadForm);
+    expect(component.errorMessage).toEqual(mockErrorMessage);
   });
 });
