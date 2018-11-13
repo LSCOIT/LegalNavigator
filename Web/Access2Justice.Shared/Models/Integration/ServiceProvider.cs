bb14dba4e700685b622f83807da250dd149bc039
Andrew Roubin <1211373+aroubin@users.noreply.github.com>
2018-11-12 17:00:39 -0800

    Adding externalId attribute to service provider

--- Web/Access2Justice.Shared/Models/Integration/ServiceProvider.cs
+++ Web/Access2Justice.Shared/Models/Integration/ServiceProvider.cs
@@ -7,6 +7,8 @@ namespace Access2Justice.Shared.Models.Integration
 
     public class ServiceProvider : Organization
     {
+        public string ExternalId { get; set; }
+
         public Availability Availability { get; set; }
 
         public AcceptanceCriteria AcceptanceCriteria { get; set; }
