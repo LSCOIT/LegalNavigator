9c63ac6781a432f59ff54bb0a2e73cecceeb1666
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-11-16 15:05:59 +0530

    changed in profile component spec

--- Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.spec.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/profile/profile.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProfileComponent } from './profile.component';
 import { PersonalizedPlanService } from '../guided-assistant/personalized-plan/personalized-plan.service';
@@ -15,8 +15,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
 import { of } from 'rxjs/observable/of';
 import { environment } from '../../environments/environment.prod';
+import { expand } from 'rxjs/operator/expand';
 
-describe('component:profile', () => {
+fdescribe('component:profile', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
   let arrayutilityservice: ArrayUtilityService;
@@ -65,6 +66,102 @@ describe('component:profile', () => {
   let mockPersonalizedPlanResources = { resources: mockresources, topics: mocktopics, webResources: mockwebresources };
   let mockUserSavedResources = [{
     "id": "1fb1b006-a8bc-487d-98a0-2457d9d9f78d",
+    "topics": [
+      {
+        "topicId": "d1d5f7a0-f1fa-464f-8da6-c2e7ce1501ef",
+        "name": "Divorce",
+        "quickLinks": [
+          {
+            "text": "Filing for Dissolution or Divorce - Ending Your Marriage",
+            "url": "http://courts.alaska.gov/shc/family/shcstart.htm#issues"
+          }
+        ],
+        "icon": "https://cs4892808efec24x447cx944.blob.core.windows…/static-resource/assets/images/topics/housing.svg",
+        "steps": [
+          {
+            "stepId": "f05ace00-c1cc-4618-a224-56aa4677d2aa",
+            "title": "File a motion to modify if there has been a change of circumstances.",
+            "description": "The Motion and Affidavit for Post-Decree relief are used to request changes to a divorce or make sure the other side is followin gthe orders.",
+            "order": 1,
+            "isComplete": false,
+            "resources": [
+              {
+                "id": "19a02209-ca38-4b74-bd67-6ea941d41518",
+                "resourceType": "Organizations"
+              },
+              {
+                "id": "9ca4cf73-f6c0-4f63-a1e8-2a3774961df5",
+                "resourceType": "Articles"
+              }
+            ]
+          },
+          {
+            "description": "Jurisdiction is a very complicated subject and you should talk to an attorney to figure out where is the best place ot file your case. Here are some resources that could help you with this:",
+            "isComplete": true,
+            "order": 2,
+            "resources": [
+              {
+                "id": "19a02209-ca38-4b74-bd67-6ea941d41518",
+                "resourceType": "Organizations"
+              },
+              {
+                "id": "9ca4cf73-f6c0-4f63-a1e8-2a3774961df5",
+                "resourceType": "Articles"
+              }
+            ],
+            "stepId": "f05ace00-c1cc-4618-a224-56aa4677d2aa",
+            "title": "Jurisdiction"
+          }
+        ]
+      },
+      {
+        "topicId": "e1fdbbc6-d66a-4275-9cd2-2be84d303e12",
+        "name": "Eviction",
+        "quickLinks": [
+          {
+            "text": "Filing for Dissolution or Divorce - Ending Your Marriage",
+            "url": "http://courts.alaska.gov/shc/family/shcstart.htm#issues"
+          }
+        ],
+        "icon": "https://cs4892808efec24x447cx944.blob.core.windows…/static-resource/assets/images/topics/housing.svg",
+        "steps": [
+          {
+            "stepId": "f79305c1-8767-4485-9e9b-0b5a573ea7b3",
+            "title": "File a motion to modify if there has been a change of circumstances.",
+            "description": "The Motion and Affidavit for Post-Decree relief are used to request changes to a divorce or make sure the other side is followin gthe orders.",
+            "order": 1,
+            "isComplete": false,
+            "resources": [
+              {
+                "id": "9ca4cf73-f6c0-4f63-a1e8-2a3774961df5",
+                "resourceType": "Forms"
+              },
+              {
+                "id": "49779468-1fe0-4183-850b-ff365e05893e",
+                "resourceType": "Organizations"
+              }
+            ]
+          },
+          {
+            "stepId": "e9337765-81fc-4d10-8850-8e872cde4ee8",
+            "title": "Jurisdiction",
+            "description": "Jurisdiction is a very complicated subject and you should talk to an attorney to figure out where is the best place ot file your case. Here are some resources that could help you with this:",
+            "order": 2,
+            "isComplete": false,
+            "resources": [
+              {
+                "id": "be0cb3e1-7054-403a-baac-d119ea5be007",
+                "resourceType": "Articles"
+              },
+              {
+                "id": "2fe9f117-bfb5-469f-b80c-877640a29f75",
+                "resourceType": "Forms"
+              }
+            ]
+          }
+        ]
+      }
+    ],
     "resources": [
       {
         "itemId": "d1d5f7a0-f1fa-464f-8da6-c2e7ce1501ef",
@@ -352,9 +449,14 @@ describe('component:profile', () => {
 
   it('should not call loginRedirect msal service method and assign values of logged in user details in ngOninit', () => {
     msalService.getUser.and.returnValue(of(mockUserData));
+    spyOn(component, 'getPersonalizedPlan');
+    //mockGlobal.isShared = true;
+    //mockIsShared = true;
     component.ngOnInit();
     expect(component.userId).toEqual(mockSharedUserId);
     expect(component.userName).toEqual(mockSharedUserName);
+    expect(component.getPersonalizedPlan).toHaveBeenCalled();
+    expect(component.showRemove).toBeTruthy();
   });
 
   it('should assign component values in getSavedResource method', () => {
