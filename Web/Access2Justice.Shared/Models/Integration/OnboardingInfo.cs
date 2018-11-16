b25c89613c74e17eb176bf18edd064d380cc29c0
Andrew Roubin <1211373+aroubin@users.noreply.github.com>
2018-11-16 14:39:36 -0800

    Updating onboarding info data contract for capturing delivery information.

--- Web/Access2Justice.Shared/Models/Integration/OnboardingInfo.cs
+++ Web/Access2Justice.Shared/Models/Integration/OnboardingInfo.cs
@@ -4,8 +4,44 @@ using System.Text;
 
 namespace Access2Justice.Shared.Models.Integration
 {
+    /// <summary>
+    /// Represents onboarding information that service provider can accept.
+    /// </summary>
     public class OnboardingInfo
     {
+        /// <summary>
+        /// Name / value pairs of data that end user can provide when
+        /// onboarding with service provider
+        /// </summary>
+        /// <example>
+        /// Age: 18-35
+        /// Gender: Female
+        /// </example>
         Field [] UserFields { get; set; }
+
+        /// <summary>
+        /// Delivery methods that service provider supports 
+        /// for accepting onboarding information.
+        /// </summary>
+        DeliveryMethod DeliveryMethod { get; set; }
+
+        /// <summary>
+        /// Destination for sending onboarding info
+        /// </summary>
+        /// <example>
+        /// http://example.com/onboarding-submission/ (web service submissions)
+        /// mailto:inbox@example.com (email submissions)
+        /// tel:123-456-7890 (fax submissions)
+        /// </example>
+        Uri DeliveryDestination { get; set; }
+
+    }
+
+    public enum DeliveryMethod
+    {
+        WebApi,
+        Email,
+        Fax,
+        AvianCarrier
     }
 }
