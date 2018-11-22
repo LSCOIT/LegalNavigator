458a7c618166e614871352e35c0871ed9d3cab51
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-11-22 19:01:25 +0530

    test data added

--- Web/Tests/Access2Justice.Api.Tests/BusinessLogic/ShareBuisnessLogicTests.cs
+++ Web/Tests/Access2Justice.Api.Tests/BusinessLogic/ShareBuisnessLogicTests.cs
@@ -17,130 +17,130 @@ using Xunit;
 
 namespace Access2Justice.Api.Tests.BusinessLogic
 {
-	public class ShareBuisnessLogicTests
-	{
-		private readonly IBackendDatabaseService dbService;
-		private readonly ICosmosDbSettings dbSettings;
-		private readonly IDynamicQueries dynamicQueries;
-		private readonly IShareSettings dbShareSettings;
-		private readonly IUserProfileBusinessLogic userProfileBusinessLogic;
-		private readonly IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic;
-		private readonly ShareBusinessLogic shareBusinessLogic;
+    public class ShareBuisnessLogicTests
+    {
+        private readonly IBackendDatabaseService dbService;
+        private readonly ICosmosDbSettings dbSettings;
+        private readonly IDynamicQueries dynamicQueries;
+        private readonly IShareSettings dbShareSettings;
+        private readonly IUserProfileBusinessLogic userProfileBusinessLogic;
+        private readonly IPersonalizedPlanBusinessLogic personalizedPlanBusinessLogic;
+        private readonly ShareBusinessLogic shareBusinessLogic;
 
-		public ShareBuisnessLogicTests()
-		{
-			dbService = Substitute.For<IBackendDatabaseService>();
-			dbSettings = Substitute.For<ICosmosDbSettings>();
-			dynamicQueries = Substitute.For<IDynamicQueries>();
-			dbShareSettings = Substitute.For<IShareSettings>();
-			userProfileBusinessLogic = Substitute.For<IUserProfileBusinessLogic>();
-			personalizedPlanBusinessLogic = Substitute.For<IPersonalizedPlanBusinessLogic>();
-			shareBusinessLogic = new ShareBusinessLogic(dynamicQueries, dbSettings, dbService, dbShareSettings, userProfileBusinessLogic, personalizedPlanBusinessLogic);
+        public ShareBuisnessLogicTests()
+        {
+            dbService = Substitute.For<IBackendDatabaseService>();
+            dbSettings = Substitute.For<ICosmosDbSettings>();
+            dynamicQueries = Substitute.For<IDynamicQueries>();
+            dbShareSettings = Substitute.For<IShareSettings>();
+            userProfileBusinessLogic = Substitute.For<IUserProfileBusinessLogic>();
+            personalizedPlanBusinessLogic = Substitute.For<IPersonalizedPlanBusinessLogic>();
+            shareBusinessLogic = new ShareBusinessLogic(dynamicQueries, dbSettings, dbService, dbShareSettings, userProfileBusinessLogic, personalizedPlanBusinessLogic);
 
-			dbSettings.ProfilesCollectionId.Returns("UserProfile");
-			dbSettings.UserResourcesCollectionId.Returns("UserResource");
-			dbShareSettings.PermaLinkMaxLength.Returns(7);
-		}
+            dbSettings.ProfilesCollectionId.Returns("UserProfile");
+            dbSettings.UserResourcesCollectionId.Returns("UserResource");
+            dbShareSettings.PermaLinkMaxLength.Returns(7);
+        }
 
-		[Theory]
-		[MemberData(nameof(ShareTestData.ShareInputData), MemberType = typeof(ShareTestData))]
-		public void CheckPermaLinkDataAsyncShouldValidate(ShareInput shareInput, dynamic expectedResult)
-		{
-            dynamic profileResponse = userProfileBusinessLogic.GetUserProfileDataAsync(shareInput.UserId, false).Returns<dynamic>(ShareTestData.UserProfileWithSharedResourceData);			
-			var dbResponse = dynamicQueries.FindItemsWhereAsync(dbSettings.UserResourcesCollectionId, "SharedResourceId", "0568B88C-3866-4CCA-97C8-B8E3F3D1FF3C");
-			dbResponse.ReturnsForAnyArgs(ShareTestData.sharedResourcesData);
+        [Theory]
+        [MemberData(nameof(ShareTestData.ShareInputData), MemberType = typeof(ShareTestData))]
+        public void CheckPermaLinkDataAsyncShouldValidate(ShareInput shareInput, dynamic expectedResult)
+        {
+            dynamic profileResponse = userProfileBusinessLogic.GetUserProfileDataAsync(shareInput.UserId, false).Returns<dynamic>(ShareTestData.UserProfileWithSharedResourceData);
+            var dbResponse = dynamicQueries.FindItemsWhereAsync(dbSettings.UserResourcesCollectionId, "SharedResourceId", "0568B88C-3866-4CCA-97C8-B8E3F3D1FF3C");
+            dbResponse.ReturnsForAnyArgs(ShareTestData.sharedResourcesData);
 
-			//act
-			var response = shareBusinessLogic.CheckPermaLinkDataAsync(shareInput);
-			expectedResult = JsonConvert.SerializeObject(expectedResult);
-			var actualResult = JsonConvert.SerializeObject(response.Result);
-			//assert
-			Assert.Equal(expectedResult, actualResult);
-		}
+            //act
+            var response = shareBusinessLogic.CheckPermaLinkDataAsync(shareInput);
+            expectedResult = JsonConvert.SerializeObject(expectedResult);
+            var actualResult = JsonConvert.SerializeObject(response.Result);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
 
         [Theory]
         [MemberData(nameof(ShareTestData.ShareInputData), MemberType = typeof(ShareTestData))]
         public void CheckPermaLinkDataAsyncShouldValidateNull(ShareInput shareInput, dynamic expectedResult)
-        {          
+        {
             shareInput.UserId = null;
             shareInput.Url = null;
             //act
             var response = shareBusinessLogic.CheckPermaLinkDataAsync(shareInput);
-            expectedResult =  JsonConvert.SerializeObject(null);
+            expectedResult = JsonConvert.SerializeObject(null);
             var actualResult = JsonConvert.SerializeObject(response.Result);
             //assert
             Assert.Contains(expectedResult, actualResult);
         }
-      
+
         [Theory]
-		[MemberData(nameof(ShareTestData.ShareGenerateInputData), MemberType = typeof(ShareTestData))]
-		public void ShareResourceDataAsyncShouldReturnResourceUrl(ShareInput shareInput, int permaLinkOutputLength, dynamic expectedResult)
-		{
+        [MemberData(nameof(ShareTestData.ShareGenerateInputData), MemberType = typeof(ShareTestData))]
+        public void ShareResourceDataAsyncShouldReturnResourceUrl(ShareInput shareInput, int permaLinkOutputLength, dynamic expectedResult)
+        {
             dynamic profileResponse = userProfileBusinessLogic.GetUserProfileDataAsync(shareInput.UserId).Returns<dynamic>(ShareTestData.UserProfileWithoutSharedResourceData);
-			dbShareSettings.PermaLinkMaxLength.Returns(permaLinkOutputLength);
+            dbShareSettings.PermaLinkMaxLength.Returns(permaLinkOutputLength);
 
-			Document updatedDocument = new Document();
-			JsonTextReader reader = new JsonTextReader(new StringReader(ShareTestData.userProfileWithSharedResource));
-			updatedDocument.LoadFrom(reader);
+            Document updatedDocument = new Document();
+            JsonTextReader reader = new JsonTextReader(new StringReader(ShareTestData.userProfileWithSharedResource));
+            updatedDocument.LoadFrom(reader);
 
-			dbService.UpdateItemAsync<UserProfile>(
-			   Arg.Any<string>(),
-			   Arg.Any<UserProfile>(),
-			   Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
+            dbService.UpdateItemAsync<UserProfile>(
+               Arg.Any<string>(),
+               Arg.Any<UserProfile>(),
+               Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
 
-			dbService.CreateItemAsync<SharedResources>(
-			   Arg.Any<SharedResources>(),
-			   Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
+            dbService.CreateItemAsync<SharedResources>(
+               Arg.Any<SharedResources>(),
+               Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
 
-			//act
-			var result = shareBusinessLogic.ShareResourceDataAsync(shareInput).Result;
-			//assert
-			Assert.Equal(expectedResult.PermaLink.Length, result.PermaLink.Length);
-		}
+            //act
+            var result = shareBusinessLogic.ShareResourceDataAsync(shareInput).Result;
+            //assert
+            Assert.Equal(expectedResult.PermaLink.Length, result.PermaLink.Length);
+        }
 
-		[Theory]
-		[MemberData(nameof(ShareTestData.UnShareInputData), MemberType = typeof(ShareTestData))]
-		public void UnshareResourceDataAsyncShouldReturnTrue(ShareInput unShareInput, dynamic expectedResult)
-		{
-			var profileResponse = userProfileBusinessLogic.GetUserProfileDataAsync(unShareInput.UserId);
-			profileResponse.ReturnsForAnyArgs(ShareTestData.UserProfileWithSharedResourceData);
+        [Theory]
+        [MemberData(nameof(ShareTestData.UnShareInputData), MemberType = typeof(ShareTestData))]
+        public void UnshareResourceDataAsyncShouldReturnTrue(ShareInput unShareInput, dynamic expectedResult)
+        {
+            var profileResponse = userProfileBusinessLogic.GetUserProfileDataAsync(unShareInput.UserId);
+            profileResponse.ReturnsForAnyArgs(ShareTestData.UserProfileWithSharedResourceData);
 
-			Document updatedDocument = new Document();
-			JsonTextReader reader = new JsonTextReader(new StringReader(ShareTestData.updatedSharedResourcesData));
-			updatedDocument.LoadFrom(reader);
+            Document updatedDocument = new Document();
+            JsonTextReader reader = new JsonTextReader(new StringReader(ShareTestData.updatedSharedResourcesData));
+            updatedDocument.LoadFrom(reader);
 
-			dbService.UpdateItemAsync<SharedResources>(
-			   Arg.Any<string>(),
-			   Arg.Any<SharedResources>(),
-			   Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
-			var dbResponse = dynamicQueries.FindItemsWhereAsync(dbSettings.UserResourcesCollectionId, "SharedResourceId", "0568B88C-3866-4CCA-97C8-B8E3F3D1FF3C");
-			dbResponse.ReturnsForAnyArgs(ShareTestData.sharedResourcesData);
+            dbService.UpdateItemAsync<SharedResources>(
+               Arg.Any<string>(),
+               Arg.Any<SharedResources>(),
+               Arg.Any<string>()).ReturnsForAnyArgs<Document>(updatedDocument);
+            var dbResponse = dynamicQueries.FindItemsWhereAsync(dbSettings.UserResourcesCollectionId, "SharedResourceId", "0568B88C-3866-4CCA-97C8-B8E3F3D1FF3C");
+            dbResponse.ReturnsForAnyArgs(ShareTestData.sharedResourcesData);
 
-			//act
-			var result = shareBusinessLogic.UnshareResourceDataAsync(unShareInput).Result;
-			expectedResult = JsonConvert.SerializeObject(expectedResult);
-			var actualResult = JsonConvert.SerializeObject(result);
-			//assert
-			Assert.Equal(expectedResult, actualResult);
-		}
+            //act
+            var result = shareBusinessLogic.UnshareResourceDataAsync(unShareInput).Result;
+            expectedResult = JsonConvert.SerializeObject(expectedResult);
+            var actualResult = JsonConvert.SerializeObject(result);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
 
-		[Theory]
-		[MemberData(nameof(ShareTestData.ShareProfileResponseData), MemberType = typeof(ShareTestData))]
-		public void GetPermaLinkDataAsyncShouldValidate(string permaLinkInput,string planId,JArray shareProfileDetails,JArray shareProfileResponse, dynamic expectedResult)
-		{
-			var dbResponse = dynamicQueries.FindFieldWhereArrayContainsAsync(dbSettings.ProfilesCollectionId, Constants.SharedResource,
-						Constants.PermaLink, permaLinkInput, Constants.ExpirationDate);
-			dbResponse.ReturnsForAnyArgs(shareProfileDetails);
+        [Theory]
+        [MemberData(nameof(ShareTestData.ShareProfileResponseData), MemberType = typeof(ShareTestData))]
+        public void GetPermaLinkDataAsyncShouldValidate(string permaLinkInput, string planId, JArray shareProfileDetails, JArray shareProfileResponse, dynamic expectedResult)
+        {
+            var dbResponse = dynamicQueries.FindFieldWhereArrayContainsAsync(dbSettings.ProfilesCollectionId, Constants.SharedResource,
+                        Constants.PermaLink, permaLinkInput, Constants.ExpirationDate);
+            dbResponse.ReturnsForAnyArgs(shareProfileDetails);
 
             var profileResponse = dynamicQueries.FindFieldWhereArrayContainsAsync(dbSettings.ProfilesCollectionId, Constants.sharedResourceId, planId);
             profileResponse.ReturnsForAnyArgs(shareProfileResponse);
 
             var response = shareBusinessLogic.GetPermaLinkDataAsync(permaLinkInput);
-			expectedResult = JsonConvert.SerializeObject(expectedResult);
-			var actualResult = JsonConvert.SerializeObject(response.Result);
-			//assert
-			Assert.Equal(expectedResult, actualResult);
-		}
+            expectedResult = JsonConvert.SerializeObject(expectedResult);
+            var actualResult = JsonConvert.SerializeObject(response.Result);
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
 
         [Theory]
         [MemberData(nameof(ShareTestData.UserProfileWithSharedResourceDataForUpdate), MemberType = typeof(ShareTestData))]
@@ -165,6 +165,27 @@ namespace Access2Justice.Api.Tests.BusinessLogic
 
             //act
             var response = shareBusinessLogic.UpsertSharedResource(userProfile, sharedResource);
+            var actualResult = JsonConvert.SerializeObject(response.Result);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [MemberData(nameof(ShareTestData.ShareGenerateInputDataIsShared), MemberType = typeof(ShareTestData))]
+        public void UpdatePlanIsSharedStatusValidate(ShareInput shareInput, bool isSharedStatus, dynamic expectedResult)
+        {
+            //arrange
+            string planId = shareInput.Url.OriginalString.Substring(8);            
+
+            dynamic personalizedPlan = personalizedPlanBusinessLogic.GetPersonalizedPlanAsync(Guid.Parse(planId))
+            .Returns<PersonalizedPlanViewModel>(ShareTestData.PersonalizedPlanViewModelDataIsShared);
+
+            // to improve below
+           // dbPersonalizedPlan.UpsertPersonalizedPlanAsync(plan)
+
+            //act
+            var response =shareBusinessLogic.UpdatePlanIsSharedStatus(planId, isSharedStatus);
             var actualResult = JsonConvert.SerializeObject(response.Result);
 
             //assert
