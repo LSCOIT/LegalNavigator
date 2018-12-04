1e3bf69bf501486f249d1b7dff7b373b7b926a08
Winnie <yfz.winnie@gmail.com>
2018-12-03 17:01:24 -0800

    added ability to add new faqs to help & faq admin template

--- Web/Access2Justice.Web/ClientApp/src/app/admin/help-faqs/help-faqs-template.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/admin/help-faqs/help-faqs-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'admin-help-faqs-template',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help-faqs-template.component.css']
 })
 export class HelpFaqsTemplateComponent implements OnInit {
+  public faqForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  get faqs() {
+    return this.faqForm.get('faqs') as FormArray;
+  }
+
+  createFAQs(): FormGroup {
+    return this.fb.group({
+      question:'',
+      answer: ''
+    });
+  }
+
+  addFAQs() {
+    this.faqs.push(this.createFAQs());
+  }
+
+  onSubmit(homeForm: NgForm) {
+    console.log(this.faqForm);
+  }
 
   ngOnInit() {
+    this.faqForm = this.fb.group({
+      faqs: this.fb.array([this.createFAQs()])
+    });
   }
 
 }
