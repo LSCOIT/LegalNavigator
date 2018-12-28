aeac308d9dc58db2f2803a9f74c92652fdb57797
Winnie <yfz.winnie@gmail.com>
2018-12-28 16:20:57 -0500

    adjusted breadcrumb for resource page when navigating from search

--- Web/Access2Justice.Web/ClientApp/src/app/topics-resources/breadcrumb/breadcrumb.component.ts
+++ Web/Access2Justice.Web/ClientApp/src/app/topics-resources/breadcrumb/breadcrumb.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, NavigationStart } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from '../shared/breadcrumb.service';
-import { Global } from '../../global';
-import { NavigateDataService } from '../../shared/navigate-data.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -16,72 +13,51 @@ export class BreadcrumbComponent implements OnInit {
   isResource: boolean = false;
   activeTopicName: string;
   activeTopicId: string;
-  isSearch: boolean = false;
-  isBookmarked: boolean = false;
+  @Input() resourceTopic: string;
+  @Input() resourceType: string;
 
-  constructor(private breadcrumbService: BreadcrumbService,
-    private activeRoute: ActivatedRoute,
-    private global: Global,
-    private navigateDataService: NavigateDataService,
-    private router: Router) {
-  }
+  constructor(
+    private breadcrumbService: BreadcrumbService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    if (this.global.previosUrl.indexOf("/search") !== -1) {
-      this.isSearch = true;
-      this.getBreadcrumbDetails();
-    }
-    else {
-      this.activeRoute.url
-        .subscribe(routeParts => {
-          for (let i = 1; i < routeParts.length; i++) {
-            this.getBreadcrumbDetails();
-          }
-        });
-    }
+    this.activeRoute.url
+      .subscribe(routeParts => {
+        for (let i = 1; i < routeParts.length; i++) {
+          this.getBreadcrumbDetails();
+        }
+      });
   }
 
   getBreadcrumbDetails() {
-    this.activeTopic = this.activeRoute.snapshot.params['topic'] ? this.activeRoute.snapshot.params['topic'] : this.global.activeSubtopicParam;
-    if (this.activeTopic != undefined) {
-      sessionStorage.setItem("ActiveTopic", this.activeTopic);
-    }
-    else {
-      this.activeTopic = sessionStorage.getItem("ActiveTopic");
-    }
-    if (this.activeTopic == null) { this.isBookmarked = true; }
+    if (this.activeRoute.snapshot.params['topic'] != null) {
+      this.activeTopic = this.activeRoute.snapshot.params['topic'];
+    } else if(this.resourceTopic) {
+      this.activeTopic = this.resourceTopic;
+    };
+
     if (this.activeRoute.snapshot.params['id'] != null) {
       this.isResource = true;
     };
 
-    if (!this.isSearch) {
-      //Get the data for topic from breadcrumb service
-      this.breadcrumbService.getBreadcrumbs(this.activeTopic)
-        .subscribe(
-          items => {
-            this.breadcrumbs = items.response.reverse();
-            this.breadcrumbs.unshift({ id: "", name: "All Topics" });
-            if (this.isResource) {
-              this.breadcrumbs.forEach(item => {
-                if (item.id[0] === this.activeTopic) {
-                  item.id = item.id[0];
-                }
-              });
-              this.breadcrumbs.push({ id: this.activeRoute.snapshot.params['id'], name: "Resources" });
-              this.activeTopic = this.activeRoute.snapshot.params['id'];
+    this.breadcrumbService.getBreadcrumbs(this.activeTopic)
+      .subscribe(
+        items => {
+          this.breadcrumbs = items.response.reverse();
+          this.breadcrumbs.unshift({ id: "", name: "All Topics" });
+          this.breadcrumbs.forEach(item => {
+            if (item.id[0] === this.activeTopic) {
+              this.activeTopicName = item.name;
+              this.activeTopicId = item.id;
             }
-            this.breadcrumbs.forEach(item => {
-              if (item.id[0] === this.activeTopic) {
-                this.activeTopicName = item.name;
-                this.activeTopicId = item.id;
-              }
-            });
           });
-    }
-    else {
-      this.breadcrumbs.unshift({ id: "", name: "Search" });
-      if (this.isResource)
-        this.breadcrumbs.push({ id: this.activeRoute.snapshot.params['id'], name: "Resources" });
-    }
+          if (this.isResource) {
+            if (this.resourceType.slice(-1) === "s") {
+              this.resourceType = this.resourceType.slice(0, this.resourceType.length - 1);
+            }
+            this.breadcrumbs.push({ id: this.resourceTopic, name: this.resourceType });
+          }
+        });
   }
 }
