91001b963ffa17964c98c6c401677be0fbf08655
Alaa Tadmori <alaa.tadmori@avanade.com>
2019-01-11 13:12:09 -0500

    basic implemenation - app reads settings from a config file | it creates a db and one collection

---
+++ Tools/A2JDbCreator/A2JDbCreator/Models/DbCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2JDbCreator.Models
+{
+    public class DbCollection
+    {
+        public string Name { get; set; }
+        public string PartitionKey { get; set; }
+        public int Throughput { get; set; }
+    }
+}
---
+++ Tools/A2JDbCreator/A2JDbCreator/Program.cs
@@ -0,0 +1,58 @@
+﻿using A2JDbCreator.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace A2JDbCreator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // setup configurations
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine("Starting DB creation -");
+
+            // read configs
+            var dbSettings = new DbSettings();
+            configuration.GetSection("DbSettings").Bind(dbSettings);
+            var dbCollections = new List<DbCollection>();
+            configuration.GetSection("DbCollections").Bind(dbCollections);
+
+            // run the creator
+            CreateAsync(dbSettings, dbCollections);
+
+            Console.WriteLine("All DBs were created successfully. Click any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static async System.Threading.Tasks.Task CreateAsync(DbSettings dbSettings, List<DbCollection> collections)
+        {
+            using (var client = new DocumentClient(new Uri(dbSettings.Endpoint), dbSettings.AuthKey))
+            {
+                var db = new Database { Id = dbSettings.Name };
+                await client.CreateDatabaseAsync(db);
+
+                var coll = new DocumentCollection { Id = "Resources" };
+                coll.PartitionKey.Paths.Add("/organizationalUnit");
+
+                var dbUri = UriFactory.CreateDatabaseUri("DevDb");
+                await client.CreateDocumentCollectionAsync(dbUri, coll, new RequestOptions { OfferThroughput = 20000 });
+            }
+            Console.WriteLine(dbSettings.Name);
+            Console.WriteLine(collections.First().Name);
+
+
+            Console.WriteLine();
+        }
+    }
+}
