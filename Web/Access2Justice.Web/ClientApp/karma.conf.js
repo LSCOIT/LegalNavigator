faefd9918c697eeb537be24913a7f8089c60e2c0
Andrei Saperski <asaperski@ARTGROUP.local>
2019-01-23 11:46:56 +0300

    added support of junit reprter for the client app tests

--- Web/Access2Justice.Web/ClientApp/karma.conf.js
+++ Web/Access2Justice.Web/ClientApp/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
@@ -22,7 +23,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
