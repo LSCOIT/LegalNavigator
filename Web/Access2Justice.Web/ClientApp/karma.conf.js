f31af9925756e1b33cd22df34e6d26ac6e605128
Andrei Saperski <asaperski@gloriumtech.com>
2019-01-29 15:08:04 +0300

    Revert "added support of junit reprter for the client app tests"
    This reverts commit faefd9918c697eeb537be24913a7f8089c60e2c0.

--- Web/Access2Justice.Web/ClientApp/karma.conf.js
+++ Web/Access2Justice.Web/ClientApp/karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
@@ -23,7 +22,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
