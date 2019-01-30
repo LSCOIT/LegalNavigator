fc42a7aa611c99282d17d0a4fff79c47e0d1bc64
Alaa Tadmori <alaa.tadmori@avanade.com>
2019-01-30 00:09:55 -0500

    fixed partitioning issue in the curated experience uploader | refactored appsettings to make it easier to run/deploy the api/web against different envrionments.

--- Web/Access2Justice.Shared/Interfaces/A2JAuthor/ICuratedExperienceConvertor.cs
+++ Web/Access2Justice.Shared/Interfaces/A2JAuthor/ICuratedExperienceConvertor.cs
@@ -1,10 +1,11 @@
 ﻿using Access2Justice.Shared.Models;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace Access2Justice.Shared.Interfaces.A2JAuthor
 {
     public interface ICuratedExperienceConvertor
     {
-        CuratedExperience ConvertA2JAuthorToCuratedExperience(JObject a2jSchema,bool isFromAdminImport = false);
+        CuratedExperience ConvertA2JAuthorToCuratedExperience(JObject a2jSchema,bool isFromAdminImport = false, Guid a2jPersonalizedPlanId = default(Guid));
     }
 }
