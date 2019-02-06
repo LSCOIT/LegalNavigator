0cdd266eb4ad6a264f546f89097a3163b1004c60
Andrei Saperski <asaperski@gloriumtech.com>
2019-02-06 12:07:57 +0300

    #830 added env files for dev, stage and prod

---
+++ Web/Access2Justice.Web/ClientApp/src/scripts/env.prod.js
@@ -0,0 +1,26 @@
+(function () {
+    Object.defineProperty(window, '__ngEnv', {
+        value: {
+            production: true,
+            apiUrl: "https://api.legalnav.org/api",
+            bingmap_key: "AirzY_3r9_Fk3oHTIEDjlBV_7qXRineubclytdXOTnmpAOWdNVv4H2sYj0nr8KPo",
+            map_type: false,
+            internalResourcePagesToShow: 2,
+            internalResourceRecordsToDisplay: 10,
+            webResourcePagesToShow: 10,
+            webResourceRecordsToDisplay: 10,
+            blobUrl: "https://legalnavprodstor.blob.core.windows.net",
+            virtualEarthUrl: "https://dev.virtualearth.net/REST/v1/Locations/",
+            All: "All",
+            clientID: "14c21c74-5579-43c3-9c55-cf5cf265d518",
+            authority: "https://login.microsoftonline.com/common/",
+            consentScopes: ["api://14c21c74-5579-43c3-9c55-cf5cf265d518/access_as_user"],
+            redirectUri: "https://www.legalnav.org/",
+            navigateToLoginRequestUrl: true,
+            postLogoutRedirectUri: "https://www.legalnav.org/",
+            topicUrl: "https://api.legalnav.org/api/topics",
+            userPersonalizedPlanUrl: "https://api.legalnav.org/api/user/upsertuserpersonalizedplan",
+            apiScope: "api://14c21c74-5579-43c3-9c55-cf5cf265d518/access_as_user"
+        }
+    });
+})();
