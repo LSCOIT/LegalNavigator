e875f4515bdb5bc4958ae3fd24b9961f7bf46f2b
Oleksandr Zholob <johngaltaregoing@gmail.com>
2019-12-13 15:28:05 +0200

    hotfix apostrophe issue

--- Web/Access2Justice.Api/BusinessLogic/TopicsResourcesBusinessLogic.cs
+++ Web/Access2Justice.Api/BusinessLogic/TopicsResourcesBusinessLogic.cs
@@ -40,6 +40,7 @@ namespace Access2Justice.Api.BusinessLogic
 
         public async Task<Topic> GetTopic(string topicName, string state = null)
         {
+            topicName = topicName.Replace("'", "’");
             List<dynamic> topics = await dbClient.FindItemsWhereAsync(dbSettings.TopicsCollectionId, Constants.Name, topicName);
             Topic topic;
             if (!topics.Any())
