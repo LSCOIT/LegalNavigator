9df09747f7c9a8a15fb72befd792edfa354e85ce
Oleksandr Zholob <johngaltaregoing@gmail.com>
2020-01-22 16:14:05 +0200

    added linq system

--- Web/Access2Justice.Api/Controllers/PersonalizedPlanController.cs
+++ Web/Access2Justice.Api/Controllers/PersonalizedPlanController.cs
@@ -1,10 +1,11 @@
-﻿using Access2Justice.Api.Interfaces;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Access2Justice.Api.Interfaces;
 using Access2Justice.Api.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
 
 namespace Access2Justice.Api.Controllers
 {
